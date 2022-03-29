14a791560544460fa40c46c823da453f2ccd2408
bzorn <v.klimenko.2137@gmail.com>
2022-03-29 16:04:35 +0300

    .

--- static.py
+++ static.py
@@ -1,5 +1,8 @@
+import os
+
 import requests
 from const import HEADERS
+from items import Manga, Chapter, Image
 
 
 def get_html(url: str, params=None):
@@ -7,3 +10,14 @@ def get_html(url: str, params=None):
         return requests.get(url, headers=HEADERS, params=params)
     except Exception as e:
         print(e)
+
+
+def get_url(manga: Manga=None, chapter: Chapter=None, image: Image=None):
+    url = f'https://desu.me/manga/api/'
+    if image:
+        return f'{os.getcwd()}/Desu/images/{manga.id}/{chapter.id}/{image.page}.jpg'
+    if manga:
+        url += f'/{manga.id}'
+        if chapter:
+            url += f'/chapter/{chapter.id}'
+    return url
