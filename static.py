67dd760d29396a50586435e063c30f226e7b9d29
bzorn <v.klimenko.2137@gmail.com>
2022-04-03 00:00:36 +0300

    .

--- static.py
+++ static.py
@@ -1,7 +1,7 @@
 import os
 
 import requests
-from const import HEADERS
+from const import HEADERS, lib_lists_en, lib_lists_ru
 from items import Manga, Chapter, Image
 
 
@@ -10,6 +10,10 @@ def get_html(url: str, params=None):
         return requests.get(url, headers=HEADERS, params=params)
     except Exception as e:
         print(e)
+
+
+def get_lib_list_en(lib_list_ru):
+    return lib_lists_en[lib_lists_ru.index(lib_list_ru)]
 
 
 def get_url(manga: Manga=None, chapter: Chapter=None, image: Image=None):
