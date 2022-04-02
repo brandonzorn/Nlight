1fabf74be7693786cf2602698ff36f04006d8ada
bzorn <v.klimenko.2137@gmail.com>
2022-04-02 19:15:58 +0300

    .

--- form_library.py
+++ form_library.py
@@ -1,14 +1,14 @@
-from PyQt5.QtCore import QObject, pyqtSignal
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QWidget
+from PySide2.QtCore import QObject, Signal
+from PySide2.QtGui import QIcon
+from PySide2.QtWidgets import QWidget
 from const import library_icon_path, main_icon_path
 from database import db
 from desu_library import Ui_Dialog
 
 
 class Communicate(QObject):
-    clicked_main = pyqtSignal()
-    double_click = pyqtSignal(object)
+    clicked_main = Signal()
+    double_click = Signal(object)
 
 
 class FormLibrary(QWidget):
