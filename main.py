c73b2ddb215d5ed9cd34900612e4617e7d8af747
bzorn <v.klimenko.2137@gmail.com>
2022-04-02 19:15:58 +0300

    .

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
 from const import app_icon_path
 from form_facial import FormFacial
 from form_info import FormInfo
