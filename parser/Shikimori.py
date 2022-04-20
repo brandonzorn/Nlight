15c8e9361bdb0b00c40c313176d7d410b238e5f0
bzorn <v.klimenko.2137@gmail.com>
2022-04-21 00:12:37 +0300

    Added parsers

--- parser/Shikimori.py
+++ parser/Shikimori.py
@@ -1,6 +1,7 @@
 from const import SHIKIMORI_HEADERS, URL_SHIKIMORI_API
-from items import Manga, Chapter, Image
+from items import Manga, Chapter, Image, Genre
 from parser.Parser import Parser
+from static import get_html
 
 
 class Shikimori(Parser):
@@ -9,11 +10,19 @@ class Shikimori(Parser):
         self.url_api = URL_SHIKIMORI_API
         self.headers = SHIKIMORI_HEADERS
 
-    def get_manga(self, manga) -> Manga:
-        pass
+    def get_manga(self, manga: Manga) -> Manga:
+        url = f'{self.url_api}/mangas/{manga.shikimori_id}'
+        html = get_html(url, self.headers)
+        if html and html.status_code == 200 and len(html.json()):
+            return Manga(html.json())
+        return manga
 
     def search_manga(self, params: dict) -> [Manga]:
-        pass
+        url = f'{self.url_api}/mangas'
+        html = get_html(url, self.headers, params)
+        if html and html.status_code == 200 and len(html.json()):
+            return [Manga(i) for i in html.json()]
+        return []
 
     def get_chapters(self, manga: Manga) -> [Chapter]:
         pass
@@ -26,3 +35,10 @@ class Shikimori(Parser):
 
     def get_preview(self, manga: Manga):
         pass
+
+    def get_genres(self):
+        url = f'{self.url_api}/genres'
+        html = get_html(url, headers=self.headers)
+        if html and html.status_code == 200 and len(html.json()):
+            return [Genre(i) for i in html.json()]
+        return []
