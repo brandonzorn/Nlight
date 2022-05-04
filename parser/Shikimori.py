bd6f55e91a9fe6eb45f4c692973e1efbdf2ed57c
bzorn <v.klimenko.2137@gmail.com>
2022-05-04 13:51:54 +0300

    Updated Authentication for Shikimori

--- parser/Shikimori.py
+++ parser/Shikimori.py
@@ -1,4 +1,8 @@
-from auth import Auth
+import oauthlib.oauth2.rfc6749.errors
+from requests_oauthlib import OAuth2Session
+from const import URL_SHIKIMORI, URL_SHIKIMORI_TOKEN
+from keys import SHIKIMORI_CLIENT_ID, SHIKIMORI_CLIENT_SECRET
+from static import token_loader, token_saver
 from const import SHIKIMORI_HEADERS, URL_SHIKIMORI_API
 from items import Manga, Chapter, Image, Genre, RequestForm, User
 from parser.Parser import Parser
@@ -79,3 +83,59 @@ class Shikimori(Parser):
                 user.avatar = data.get('avatar')
                 user.locale = data.get('locale')
         return user
+
+
+class Auth:
+    def __init__(self, token=None, scope=None):
+        self.client_id = SHIKIMORI_CLIENT_ID
+        self.client_secret = SHIKIMORI_CLIENT_SECRET
+        self.redirect_uri = 'urn:ietf:wg:oauth:2.0:oob'
+        self.extra = {'client_id': self.client_id, 'client_secret': self.client_secret}
+        self.token_saver = token_saver
+        self.tokens = token_loader(Shikimori.catalog_name)
+        self.headers = {'User-Agent': 'Shikimori', 'Authorization': f'Bearer {self.tokens.get("access_token")}'}
+        self.client = self.get_client(scope, self.redirect_uri, token)
+
+    def get_client(self, scope, redirect_uri, token):
+        client = OAuth2Session(self.client_id, auto_refresh_url=URL_SHIKIMORI_TOKEN, auto_refresh_kwargs=self.extra,
+                               scope=scope, redirect_uri=redirect_uri, token=token,
+                               token_updater=self.token_saver)
+        client.headers.update(self.headers)
+        return client
+
+    def get_auth_url(self):
+        auth_url = URL_SHIKIMORI + '/oauth/authorize'
+        return self.client.authorization_url(auth_url)[0]
+
+    def fetch_token(self, code):
+        try:
+            self.client.fetch_token(URL_SHIKIMORI_TOKEN, code, client_secret=self.client_secret)
+        except oauthlib.oauth2.rfc6749.errors.InvalidGrantError:
+            return
+        self.token_saver(self.token, Shikimori.catalog_name)
+        return self.token
+
+    def refresh_token(self):
+        if not token_loader(Shikimori.catalog_name):
+            return
+        self.client.headers.clear()
+        self.client.headers.update({'User-Agent': 'Shikimori'})
+        self.client.refresh_token(URL_SHIKIMORI_TOKEN,
+                                  refresh_token=token_loader(Shikimori.catalog_name).get('refresh_token'))
+        self.token_saver(self.token, Shikimori.catalog_name)
+        self.client.headers.update({'Authorization': f'Bearer'
+                                                     f'{token_loader(Shikimori.catalog_name).get("access_token")}'})
+        return self.token
+
+    def get(self, url, params=None):
+        return self.client.request('GET', url, params)
+
+    def check_auth(self):
+        whoami = self.get('https://shikimori.one/api/users/whoami')
+        if whoami and whoami.json():
+            return True
+        return False
+
+    @property
+    def token(self):
+        return self.client.token
