151f7d244e071d151a292f441cbc6e4e2a9ce844
bzorn <v.klimenko.2137@gmail.com>
2022-05-10 18:09:57 +0300

    Qt6

--- form_auth.py
+++ form_auth.py
@@ -1,7 +1,7 @@
 import webbrowser
 
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
 from const import app_icon_path
 from form.authUI import Ui_Dialog
 
