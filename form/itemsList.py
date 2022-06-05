a359018a2cb1579cef4b275a36a781a6c9515d68
bzorn <v.klimenko.2137@gmail.com>
2022-06-06 00:40:40 +0300

    items list widget

---
+++ form/itemsList.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'itemsList.ui'
+##
+## Created by: Qt User Interface Compiler version 6.3.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QLabel, QLineEdit,
+    QListWidget, QListWidgetItem, QPushButton, QSizePolicy,
+    QVBoxLayout, QWidget)
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(371, 560)
+        self.verticalLayout = QVBoxLayout(Form)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.list_manga = QListWidget(Form)
+        self.list_manga.setObjectName(u"list_manga")
+        palette = QPalette()
+        brush = QBrush(QColor(255, 255, 255, 255))
+        brush.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.WindowText, brush)
+        brush1 = QBrush(QColor(45, 45, 45, 255))
+        brush1.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.Button, brush1)
+        palette.setBrush(QPalette.Active, QPalette.Light, brush1)
+        palette.setBrush(QPalette.Active, QPalette.Midlight, brush1)
+        palette.setBrush(QPalette.Active, QPalette.Dark, brush1)
+        palette.setBrush(QPalette.Active, QPalette.Mid, brush1)
+        palette.setBrush(QPalette.Active, QPalette.Text, brush)
+        palette.setBrush(QPalette.Active, QPalette.ButtonText, brush)
+        palette.setBrush(QPalette.Active, QPalette.Base, brush1)
+        palette.setBrush(QPalette.Active, QPalette.Window, brush1)
+        palette.setBrush(QPalette.Active, QPalette.Shadow, brush1)
+        brush2 = QBrush(QColor(0, 133, 62, 255))
+        brush2.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.Highlight, brush2)
+        palette.setBrush(QPalette.Active, QPalette.AlternateBase, brush1)
+        palette.setBrush(QPalette.Active, QPalette.ToolTipBase, brush1)
+        brush3 = QBrush(QColor(236, 240, 241, 255))
+        brush3.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.ToolTipText, brush3)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+        palette.setBrush(QPalette.Active, QPalette.PlaceholderText, brush)
+#endif
+        palette.setBrush(QPalette.Inactive, QPalette.WindowText, brush)
+        palette.setBrush(QPalette.Inactive, QPalette.Button, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.Light, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.Midlight, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.Dark, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.Mid, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.Text, brush)
+        palette.setBrush(QPalette.Inactive, QPalette.ButtonText, brush)
+        palette.setBrush(QPalette.Inactive, QPalette.Base, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.Window, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.Shadow, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.Highlight, brush2)
+        palette.setBrush(QPalette.Inactive, QPalette.AlternateBase, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.ToolTipBase, brush1)
+        palette.setBrush(QPalette.Inactive, QPalette.ToolTipText, brush3)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+        palette.setBrush(QPalette.Inactive, QPalette.PlaceholderText, brush)
+#endif
+        palette.setBrush(QPalette.Disabled, QPalette.WindowText, brush)
+        palette.setBrush(QPalette.Disabled, QPalette.Button, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Light, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Midlight, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Dark, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Mid, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Text, brush)
+        palette.setBrush(QPalette.Disabled, QPalette.ButtonText, brush)
+        palette.setBrush(QPalette.Disabled, QPalette.Base, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Window, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Shadow, brush1)
+        brush4 = QBrush(QColor(0, 120, 215, 255))
+        brush4.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Disabled, QPalette.Highlight, brush4)
+        palette.setBrush(QPalette.Disabled, QPalette.AlternateBase, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.ToolTipBase, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.ToolTipText, brush3)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+        palette.setBrush(QPalette.Disabled, QPalette.PlaceholderText, brush)
+#endif
+        self.list_manga.setPalette(palette)
+        self.list_manga.setAutoFillBackground(False)
+
+        self.verticalLayout.addWidget(self.list_manga)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.line_search = QLineEdit(Form)
+        self.line_search.setObjectName(u"line_search")
+        self.line_search.setStyleSheet(u"background-color: rgb(45, 45, 45);\n"
+"color: rgb(255, 255, 255);")
+
+        self.horizontalLayout_2.addWidget(self.line_search)
+
+        self.btn_search = QPushButton(Form)
+        self.btn_search.setObjectName(u"btn_search")
+        self.btn_search.setStyleSheet(u"background-color: rgb(0, 133, 52);\n"
+"color: rgb(255, 255, 255);")
+
+        self.horizontalLayout_2.addWidget(self.btn_search)
+
+        self.prev_page = QPushButton(Form)
+        self.prev_page.setObjectName(u"prev_page")
+        self.prev_page.setStyleSheet(u"background-color: rgb(0, 133, 52);\n"
+"color: rgb(255, 255, 255);")
+
+        self.horizontalLayout_2.addWidget(self.prev_page)
+
+        self.label_page = QLabel(Form)
+        self.label_page.setObjectName(u"label_page")
+
+        self.horizontalLayout_2.addWidget(self.label_page)
+
+        self.next_page = QPushButton(Form)
+        self.next_page.setObjectName(u"next_page")
+        self.next_page.setStyleSheet(u"background-color: rgb(0, 133, 52);\n"
+"color: rgb(255, 255, 255);")
+
+        self.horizontalLayout_2.addWidget(self.next_page)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.btn_search.setText(QCoreApplication.translate("Form", u"\u041f\u043e\u0438\u0441\u043a", None))
+        self.prev_page.setText(QCoreApplication.translate("Form", u"<", None))
+        self.label_page.setText(QCoreApplication.translate("Form", u" \u0421\u0442\u0440\u0430\u043d\u0438\u0446\u0430 1", None))
+        self.next_page.setText(QCoreApplication.translate("Form", u">", None))
+    # retranslateUi
+
