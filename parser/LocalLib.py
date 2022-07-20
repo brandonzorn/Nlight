bb18f6661c0d123d9465efe586c662beb2242df5
bzorn <v.klimenko.2137@gmail.com>
2022-07-20 20:23:48 +0300

    added local library catalog

---
+++ parser/LocalLib.py
@@ -0,0 +1,18 @@
+from items import Manga, RequestForm
+from database import Database
+from parser.Parser import Parser
+
+
+class LocalLib(Parser):
+    catalog_name = 'LocalLib'
+
+    def __init__(self):
+        self.catalog_id = -1
+        self.db: Database = Database()
+
+    def get_manga(self, manga: Manga):
+        return manga
+
+    def search_manga(self, params: RequestForm):
+        manga = self.db.get_manga_library(params.mylist)
+        return manga
