009abd3e25dd1b333b4cd2ab15f8aa90cc076863
bzorn <v.klimenko.2137@gmail.com>
2022-07-26 22:29:43 +0300

    _

--- forms/desu_library.py
+++ forms/desu_library.py
@@ -8,16 +8,11 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QDialog, QFrame, QHBoxLayout,
-    QListWidget, QListWidgetItem, QPushButton, QSizePolicy,
-    QSpacerItem, QVBoxLayout, QWidget)
+from PySide6.QtCore import (QCoreApplication, QMetaObject, Qt)
+from PySide6.QtGui import (QBrush, QColor, QPalette)
+from PySide6.QtWidgets import (QFrame, QHBoxLayout,
+                               QListWidget, QPushButton, QSizePolicy,
+                               QSpacerItem, QVBoxLayout)
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
