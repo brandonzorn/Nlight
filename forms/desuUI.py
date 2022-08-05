5fa6657b938e5bafad4184a1d8057447d0721e0c
bzorn <v.klimenko.2137@gmail.com>
2022-08-05 14:24:17 +0300

    fixed widgets size

--- forms/desuUI.py
+++ forms/desuUI.py
@@ -24,7 +24,7 @@ class Ui_Dialog(object):
     def setupUi(self, Dialog):
         if not Dialog.objectName():
             Dialog.setObjectName(u"Dialog")
-        Dialog.resize(689, 449)
+        Dialog.resize(498, 364)
         Dialog.setStyleSheet(u"QFrame {\n"
 "	border-radius: 10px;\n"
 "	background-color: rgb(45, 45, 45);\n"
@@ -49,12 +49,18 @@ class Ui_Dialog(object):
 "QPushButton:checked {\n"
 "	border-left: 3px solid white;\n"
 "}")
-        self.horizontalLayout = QHBoxLayout(Dialog)
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.verticalLayout = QVBoxLayout()
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.list_manga = QListWidget(Dialog)
+        self.horizontalLayout_4 = QHBoxLayout(Dialog)
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.horizontalLayout_4.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_3 = QVBoxLayout()
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.items_frame = QFrame(Dialog)
+        self.items_frame.setObjectName(u"items_frame")
+        self.items_frame.setFrameShape(QFrame.StyledPanel)
+        self.items_frame.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_9 = QVBoxLayout(self.items_frame)
+        self.verticalLayout_9.setObjectName(u"verticalLayout_9")
+        self.list_manga = QListWidget(self.items_frame)
         self.list_manga.setObjectName(u"list_manga")
         palette = QPalette()
         brush = QBrush(QColor(255, 255, 255, 255))
@@ -95,73 +101,91 @@ class Ui_Dialog(object):
 #endif
         self.list_manga.setPalette(palette)
         self.list_manga.setAutoFillBackground(False)
+        self.list_manga.setStyleSheet(u"")
 
-        self.verticalLayout.addWidget(self.list_manga)
+        self.verticalLayout_9.addWidget(self.list_manga)
+
+
+        self.verticalLayout_3.addWidget(self.items_frame)
 
         self.search_frame = QFrame(Dialog)
         self.search_frame.setObjectName(u"search_frame")
-        self.search_frame.setStyleSheet(u"")
         self.search_frame.setFrameShape(QFrame.StyledPanel)
         self.search_frame.setFrameShadow(QFrame.Raised)
-        self.horizontalLayout_2 = QHBoxLayout(self.search_frame)
+        self.horizontalLayout_3 = QHBoxLayout(self.search_frame)
+        self.horizontalLayout_3.setSpacing(0)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.horizontalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.text_frame = QFrame(self.search_frame)
+        self.text_frame.setObjectName(u"text_frame")
+        self.text_frame.setFrameShape(QFrame.StyledPanel)
+        self.text_frame.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_2 = QHBoxLayout(self.text_frame)
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.horizontalLayout_4 = QHBoxLayout()
-        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
-        self.line_search = QLineEdit(self.search_frame)
+        self.line_search = QLineEdit(self.text_frame)
         self.line_search.setObjectName(u"line_search")
         self.line_search.setStyleSheet(u"background-color: rgb(45, 45, 45);\n"
 "color: rgb(255, 255, 255);")
 
-        self.horizontalLayout_4.addWidget(self.line_search)
+        self.horizontalLayout_2.addWidget(self.line_search)
 
-        self.btn_search = QPushButton(self.search_frame)
+        self.btn_search = QPushButton(self.text_frame)
         self.btn_search.setObjectName(u"btn_search")
         self.btn_search.setStyleSheet(u"background-color: rgb(0, 133, 52);")
         icon = QIcon()
         icon.addFile(u"images/search.png", QSize(), QIcon.Normal, QIcon.On)
         self.btn_search.setIcon(icon)
 
-        self.horizontalLayout_4.addWidget(self.btn_search)
+        self.horizontalLayout_2.addWidget(self.btn_search)
 
 
-        self.horizontalLayout_2.addLayout(self.horizontalLayout_4)
+        self.horizontalLayout_3.addWidget(self.text_frame)
 
-        self.horizontalLayout_3 = QHBoxLayout()
-        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.prev_page = QPushButton(self.search_frame)
+        self.page_frame = QFrame(self.search_frame)
+        self.page_frame.setObjectName(u"page_frame")
+        self.page_frame.setFrameShape(QFrame.StyledPanel)
+        self.page_frame.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout = QHBoxLayout(self.page_frame)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.prev_page = QPushButton(self.page_frame)
         self.prev_page.setObjectName(u"prev_page")
         self.prev_page.setStyleSheet(u"background-color: rgb(0, 133, 52);")
         icon1 = QIcon()
         icon1.addFile(u"images/prev.png", QSize(), QIcon.Normal, QIcon.On)
         self.prev_page.setIcon(icon1)
 
-        self.horizontalLayout_3.addWidget(self.prev_page)
+        self.horizontalLayout.addWidget(self.prev_page)
 
-        self.label_page = QLabel(self.search_frame)
+        self.label_page = QLabel(self.page_frame)
         self.label_page.setObjectName(u"label_page")
 
-        self.horizontalLayout_3.addWidget(self.label_page)
+        self.horizontalLayout.addWidget(self.label_page)
 
-        self.next_page = QPushButton(self.search_frame)
+        self.next_page = QPushButton(self.page_frame)
         self.next_page.setObjectName(u"next_page")
         self.next_page.setStyleSheet(u"background-color: rgb(0, 133, 52);")
         icon2 = QIcon()
         icon2.addFile(u"images/next.png", QSize(), QIcon.Normal, QIcon.On)
         self.next_page.setIcon(icon2)
 
-        self.horizontalLayout_3.addWidget(self.next_page)
+        self.horizontalLayout.addWidget(self.next_page)
 
 
-        self.horizontalLayout_2.addLayout(self.horizontalLayout_3)
+        self.horizontalLayout_3.addWidget(self.page_frame)
 
 
-        self.verticalLayout.addWidget(self.search_frame)
+        self.verticalLayout_3.addWidget(self.search_frame)
 
 
-        self.horizontalLayout.addLayout(self.verticalLayout)
+        self.horizontalLayout_4.addLayout(self.verticalLayout_3)
 
-        self.catalog_list = QListWidget(Dialog)
+        self.catalogs_frame = QFrame(Dialog)
+        self.catalogs_frame.setObjectName(u"catalogs_frame")
+        self.catalogs_frame.setFrameShape(QFrame.StyledPanel)
+        self.catalogs_frame.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_8 = QVBoxLayout(self.catalogs_frame)
+        self.verticalLayout_8.setObjectName(u"verticalLayout_8")
+        self.catalog_list = QListWidget(self.catalogs_frame)
         self.catalog_list.setObjectName(u"catalog_list")
         self.catalog_list.setEnabled(True)
         sizePolicy = QSizePolicy(QSizePolicy.Maximum, QSizePolicy.Preferred)
@@ -171,95 +195,57 @@ class Ui_Dialog(object):
         self.catalog_list.setSizePolicy(sizePolicy)
         palette1 = QPalette()
         palette1.setBrush(QPalette.Active, QPalette.WindowText, brush)
-        brush3 = QBrush(QColor(32, 32, 32, 255))
-        brush3.setStyle(Qt.SolidPattern)
-        palette1.setBrush(QPalette.Active, QPalette.Button, brush3)
+        palette1.setBrush(QPalette.Active, QPalette.Button, brush1)
         palette1.setBrush(QPalette.Active, QPalette.Text, brush)
         palette1.setBrush(QPalette.Active, QPalette.ButtonText, brush)
-        palette1.setBrush(QPalette.Active, QPalette.Base, brush3)
-        palette1.setBrush(QPalette.Active, QPalette.Window, brush3)
+        palette1.setBrush(QPalette.Active, QPalette.Base, brush1)
+        palette1.setBrush(QPalette.Active, QPalette.Window, brush1)
         palette1.setBrush(QPalette.Active, QPalette.Highlight, brush2)
 #if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
         palette1.setBrush(QPalette.Active, QPalette.PlaceholderText, brush)
 #endif
-        brush4 = QBrush(QColor(0, 0, 0, 255))
-        brush4.setStyle(Qt.SolidPattern)
-        palette1.setBrush(QPalette.Inactive, QPalette.WindowText, brush4)
-        brush5 = QBrush(QColor(240, 240, 240, 255))
-        brush5.setStyle(Qt.SolidPattern)
-        palette1.setBrush(QPalette.Inactive, QPalette.Button, brush5)
-        palette1.setBrush(QPalette.Inactive, QPalette.Text, brush4)
-        palette1.setBrush(QPalette.Inactive, QPalette.ButtonText, brush4)
-        palette1.setBrush(QPalette.Inactive, QPalette.Base, brush)
-        palette1.setBrush(QPalette.Inactive, QPalette.Window, brush5)
-        palette1.setBrush(QPalette.Inactive, QPalette.Highlight, brush5)
-        brush6 = QBrush(QColor(0, 0, 0, 128))
-        brush6.setStyle(Qt.SolidPattern)
+        palette1.setBrush(QPalette.Inactive, QPalette.WindowText, brush)
+        palette1.setBrush(QPalette.Inactive, QPalette.Button, brush1)
+        palette1.setBrush(QPalette.Inactive, QPalette.Text, brush)
+        palette1.setBrush(QPalette.Inactive, QPalette.ButtonText, brush)
+        palette1.setBrush(QPalette.Inactive, QPalette.Base, brush1)
+        palette1.setBrush(QPalette.Inactive, QPalette.Window, brush1)
+        brush3 = QBrush(QColor(240, 240, 240, 255))
+        brush3.setStyle(Qt.SolidPattern)
+        palette1.setBrush(QPalette.Inactive, QPalette.Highlight, brush3)
 #if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
-        palette1.setBrush(QPalette.Inactive, QPalette.PlaceholderText, brush6)
+        palette1.setBrush(QPalette.Inactive, QPalette.PlaceholderText, brush)
 #endif
         palette1.setBrush(QPalette.Disabled, QPalette.WindowText, brush)
-        palette1.setBrush(QPalette.Disabled, QPalette.Button, brush3)
+        palette1.setBrush(QPalette.Disabled, QPalette.Button, brush1)
         palette1.setBrush(QPalette.Disabled, QPalette.Text, brush)
         palette1.setBrush(QPalette.Disabled, QPalette.ButtonText, brush)
-        palette1.setBrush(QPalette.Disabled, QPalette.Base, brush3)
-        palette1.setBrush(QPalette.Disabled, QPalette.Window, brush3)
-        brush7 = QBrush(QColor(0, 120, 215, 255))
-        brush7.setStyle(Qt.SolidPattern)
-        palette1.setBrush(QPalette.Disabled, QPalette.Highlight, brush7)
+        palette1.setBrush(QPalette.Disabled, QPalette.Base, brush1)
+        palette1.setBrush(QPalette.Disabled, QPalette.Window, brush1)
+        brush4 = QBrush(QColor(0, 120, 215, 255))
+        brush4.setStyle(Qt.SolidPattern)
+        palette1.setBrush(QPalette.Disabled, QPalette.Highlight, brush4)
 #if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
-        palette1.setBrush(QPalette.Disabled, QPalette.PlaceholderText, brush6)
+        palette1.setBrush(QPalette.Disabled, QPalette.PlaceholderText, brush)
 #endif
         self.catalog_list.setPalette(palette1)
 
-        self.horizontalLayout.addWidget(self.catalog_list)
+        self.verticalLayout_8.addWidget(self.catalog_list)
 
-        self.frame_2 = QFrame(Dialog)
-        self.frame_2.setObjectName(u"frame_2")
-        self.frame_2.setStyleSheet(u"QFrame {\n"
-"	border-radius: 10px;\n"
-"	background-color: rgb(45, 45, 45);\n"
-"	color: rgb(255, 255, 255);\n"
-"}\n"
-"QDialog{\n"
-"	background-color: rgb(32, 32, 32);\n"
-"}\n"
-"\n"
-"QPushButton {\n"
-"	padding: 5px 1px;\n"
-"	border-radius: 10px;\n"
-"	font-weight: bold;\n"
-"	background-color: rgb(0, 133, 52);\n"
-"	color: rgb(255, 255, 255);\n"
-"}\n"
-"\n"
-"QPushButton:hover:!pressed {\n"
-"	border-left: 3px solid white;\n"
-"}\n"
-"\n"
-"QPushButton:checked {\n"
-"	border-left: 3px solid white;\n"
-"}\n"
-"\n"
-"QAbstractButton{\n"
-"	color: rgb(255, 255, 255);\n"
-"}")
-        self.frame_2.setFrameShape(QFrame.StyledPanel)
-        self.frame_2.setFrameShadow(QFrame.Raised)
-        self.verticalLayout_3 = QVBoxLayout(self.frame_2)
-        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
-        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
-        self.filters_frame = QFrame(self.frame_2)
+
+        self.horizontalLayout_4.addWidget(self.catalogs_frame)
+
+        self.filters_frame = QFrame(Dialog)
         self.filters_frame.setObjectName(u"filters_frame")
-        self.filters_frame.setStyleSheet(u"QAbstractButton{\n"
+        self.filters_frame.setStyleSheet(u"QWidget {\n"
+"	border-radius: 10px;\n"
 "	background-color: rgb(45, 45, 45);\n"
 "	color: rgb(255, 255, 255);\n"
 "}")
         self.filters_frame.setFrameShape(QFrame.StyledPanel)
         self.filters_frame.setFrameShadow(QFrame.Raised)
-        self.verticalLayout_6 = QVBoxLayout(self.filters_frame)
-        self.verticalLayout_6.setObjectName(u"verticalLayout_6")
-        self.verticalLayout_6.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout = QVBoxLayout(self.filters_frame)
+        self.verticalLayout.setObjectName(u"verticalLayout")
         self.order_frame = QFrame(self.filters_frame)
         self.order_frame.setObjectName(u"order_frame")
         self.order_frame.setFrameShape(QFrame.StyledPanel)
@@ -278,7 +264,7 @@ class Ui_Dialog(object):
         self.verticalLayout_2.addLayout(self.order_grid)
 
 
-        self.verticalLayout_6.addWidget(self.order_frame)
+        self.verticalLayout.addWidget(self.order_frame)
 
         self.kind_frame = QFrame(self.filters_frame)
         self.kind_frame.setObjectName(u"kind_frame")
@@ -310,7 +296,7 @@ class Ui_Dialog(object):
         self.verticalLayout_4.addLayout(self.kind_grid)
 
 
-        self.verticalLayout_6.addWidget(self.kind_frame)
+        self.verticalLayout.addWidget(self.kind_frame)
 
         self.genres_frame = QFrame(self.filters_frame)
         self.genres_frame.setObjectName(u"genres_frame")
@@ -331,56 +317,53 @@ class Ui_Dialog(object):
         self.verticalLayout_5.addWidget(self.btn_genres_list)
 
 
-        self.verticalLayout_6.addWidget(self.genres_frame)
+        self.verticalLayout.addWidget(self.genres_frame)
 
-        self.catalogs_frame = QFrame(self.filters_frame)
-        self.catalogs_frame.setObjectName(u"catalogs_frame")
-        self.catalogs_frame.setFrameShape(QFrame.StyledPanel)
-        self.catalogs_frame.setFrameShadow(QFrame.Raised)
-        self.verticalLayout_7 = QVBoxLayout(self.catalogs_frame)
+        self.filter_catalogs_frame = QFrame(self.filters_frame)
+        self.filter_catalogs_frame.setObjectName(u"filter_catalogs_frame")
+        self.filter_catalogs_frame.setFrameShape(QFrame.StyledPanel)
+        self.filter_catalogs_frame.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_7 = QVBoxLayout(self.filter_catalogs_frame)
         self.verticalLayout_7.setObjectName(u"verticalLayout_7")
         self.verticalLayout_7.setContentsMargins(0, 0, 0, 0)
-        self.btn_catalogs = QPushButton(self.catalogs_frame)
+        self.btn_catalogs = QPushButton(self.filter_catalogs_frame)
         self.btn_catalogs.setObjectName(u"btn_catalogs")
         self.btn_catalogs.setStyleSheet(u"background-color: rgb(0, 133, 52);")
 
         self.verticalLayout_7.addWidget(self.btn_catalogs)
 
 
-        self.verticalLayout_6.addWidget(self.catalogs_frame)
-
-
-        self.verticalLayout_3.addWidget(self.filters_frame)
+        self.verticalLayout.addWidget(self.filter_catalogs_frame)
 
         self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
 
-        self.verticalLayout_3.addItem(self.verticalSpacer)
+        self.verticalLayout.addItem(self.verticalSpacer)
 
-        self.frame = QFrame(self.frame_2)
-        self.frame.setObjectName(u"frame")
-        self.frame.setStyleSheet(u"")
-        self.frame.setFrameShape(QFrame.StyledPanel)
-        self.frame.setFrameShadow(QFrame.Raised)
-        self.horizontalLayout_5 = QHBoxLayout(self.frame)
+        self.actions_frame = QFrame(self.filters_frame)
+        self.actions_frame.setObjectName(u"actions_frame")
+        self.actions_frame.setStyleSheet(u"")
+        self.actions_frame.setFrameShape(QFrame.StyledPanel)
+        self.actions_frame.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_5 = QHBoxLayout(self.actions_frame)
         self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
         self.horizontalLayout_5.setContentsMargins(0, 0, 0, 0)
-        self.filter_reset = QPushButton(self.frame)
+        self.filter_reset = QPushButton(self.actions_frame)
         self.filter_reset.setObjectName(u"filter_reset")
         self.filter_reset.setStyleSheet(u"background-color: rgb(0, 133, 52);")
 
         self.horizontalLayout_5.addWidget(self.filter_reset)
 
-        self.filter_apply = QPushButton(self.frame)
+        self.filter_apply = QPushButton(self.actions_frame)
         self.filter_apply.setObjectName(u"filter_apply")
         self.filter_apply.setStyleSheet(u"background-color: rgb(0, 133, 52);")
 
         self.horizontalLayout_5.addWidget(self.filter_apply)
 
 
-        self.verticalLayout_3.addWidget(self.frame)
+        self.verticalLayout.addWidget(self.actions_frame)
 
 
-        self.horizontalLayout.addWidget(self.frame_2)
+        self.horizontalLayout_4.addWidget(self.filters_frame)
 
 
         self.retranslateUi(Dialog)
