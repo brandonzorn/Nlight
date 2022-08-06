2bc2c58c1b1733d4059e38bedb933cc0f99ae8f5
bzorn <v.klimenko.2137@gmail.com>
2022-08-06 21:52:21 +0300

    added russian names

--- const/shikimori_items.py
+++ const/shikimori_items.py
@@ -1,6 +1,6 @@
 ORDERS = [{'name': 'popularity', 'russian': 'Популярность'}, {'name': 'name', 'russian': 'Название'},
-          {'name': 'aired_on', 'russian': 'aired_on'}, {'name': 'volumes', 'russian': 'volumes'},
-          {'name': 'chapters', 'russian': 'chapters'}, {'name': 'status', 'russian': 'status'}]
+          {'name': 'aired_on', 'russian': 'Дата выхода'}, {'name': 'volumes', 'russian': 'Тома'},
+          {'name': 'chapters', 'russian': 'Главы'}, {'name': 'status', 'russian': 'Статус'}]
 
 KINDS = [{'name': 'manga', 'russian': 'Манга'}, {'name': 'manhwa', 'russian': 'Манхва'},
          {'name': 'manhua', 'russian': 'Маньхуа'}, {'name': 'one_shot', 'russian': 'Ваншот'},
