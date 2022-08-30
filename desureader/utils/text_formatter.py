75c04b358f798e25a3f1a41014782f5d0ae37d2d
bzorn <v.klimenko.2137@gmail.com>
2022-08-30 19:09:15 +0300

    added button to display spoilers in character description

--- desureader/utils/text_formatter.py
+++ desureader/utils/text_formatter.py
@@ -3,10 +3,16 @@ import re
 
 class TextFormatter:
     @staticmethod
-    def description_to_html(text: str) -> str:
+    def description_to_html(text: str, show_spoilers=False) -> str:
         if not text:
             return ""
         for ch_id, name in re.findall(r"\[character=(\w+)](.+?)\[/character]", text):
             text = text.replace(f"[character={ch_id}]{name}[/character]",
                                 f'<span style=" color:#177e00;">{name}</span>')
+        for i, spoiler_text in re.findall(r"\[spoiler=(\w+)](.+?)\[/spoiler]", text):
+            spoiler = spoiler_text
+            if not show_spoilers:
+                spoiler = ""
+            text = text.replace(f"[spoiler={i}]{spoiler_text}[/spoiler]",
+                                f'<span style=" color:#951a00;">{spoiler}</span>')
         return text
