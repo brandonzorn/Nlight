a0f1cd725403fdf41558977555c5931e81868ef0
bzorn <v.klimenko.2137@gmail.com>
2022-11-12 17:33:49 +0300

    updated read mark menu

--- nlightreader/contexts/ReadMark.py
+++ nlightreader/contexts/ReadMark.py
@@ -13,3 +13,16 @@ class ReadMarkMenu(QMenu):
         self.set_as_read = QAction('Отметить прочитанным')
         self.set_as_read_all = QAction('Отметить прочитанным все предыдущие')
         self.remove_read_state = QAction('Удалить отметку о прочтении')
+
+    def set_mode(self, mode: int):
+        match mode:
+            case 0:
+                self.addAction(self.set_as_read)
+                self.addAction(self.set_as_read_all)
+            case 1:
+                self.addAction(self.remove_read_state)
+                self.addAction(self.set_as_read_all)
+            case 2:
+                self.addAction(self.set_as_read)
+                self.addAction(self.remove_read_state)
+                self.addAction(self.set_as_read_all)
