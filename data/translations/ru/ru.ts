d586965d49c9989febd905e5735da7d87b2bcca9
bzorn <v.klimenko.2137@gmail.com>
2022-12-16 12:42:56 +0300

    fix translation sign in button

--- data/translations/ru/ru.ts
+++ data/translations/ru/ru.ts
@@ -55,6 +55,10 @@
         <source>Catalogs</source>
         <translation>Каталоги</translation>
     </message>
+    <message>
+        <source>Sign in</source>
+        <translation>Войти</translation>
+    </message>
 </context>
 
 <context>
