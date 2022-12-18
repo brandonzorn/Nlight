8c4f5133b6e4ab3704c82a6775b272652fe3d59c
bzorn <v.klimenko.2137@gmail.com>
2022-12-18 13:44:11 +0300

    updated QMenu translation

--- nlightreader/contexts/LibraryManga.py
+++ nlightreader/contexts/LibraryManga.py
@@ -1,13 +1,15 @@
 from PySide6.QtGui import QAction
 from PySide6.QtWidgets import QMenu
 
+from nlightreader.utils import translate
+
 
 class LibraryMangaMenu(QMenu):
     def __init__(self):
         super().__init__()
-        self.add_to_lib = QAction('Добавить в библиотеку')
-        self.remove_from_lib = QAction('Удалить из библиотеки')
-        self.open_in_browser = QAction('Открыть в браузере')
+        self.add_to_lib = QAction(translate("Menu", "Add to Library"))
+        self.remove_from_lib = QAction(translate("Menu", "Remove from library"))
+        self.open_in_browser = QAction(translate("Menu", "Open in browser"))
 
     def set_mode(self, mode: int):
         self.addAction(self.open_in_browser)
