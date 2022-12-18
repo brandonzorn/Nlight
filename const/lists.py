b81b7006fe270ce9750afb144fd4cbcf561af7d7
bzorn <v.klimenko.2137@gmail.com>
2022-12-18 14:21:37 +0300

    updated en and uk translations

--- const/lists.py
+++ const/lists.py
@@ -1,7 +1,6 @@
 from enum import Enum, unique
 
 lib_lists_en = ('planned', 'completed', 'watching', 'rewatching', 'on_hold', 'dropped')
-lib_lists_ru = ('запланированно', 'прочитано', 'читаю', 'перечитываю', 'отложено', 'брошено')
 
 
 @unique
