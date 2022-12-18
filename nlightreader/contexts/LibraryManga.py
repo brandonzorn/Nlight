2f04fa6399b31c4c6cc37aac38c5283160666a55
bzorn <v.klimenko.2137@gmail.com>
2022-12-18 13:17:28 +0300

    updated QMenu style

--- nlightreader/contexts/LibraryManga.py
+++ nlightreader/contexts/LibraryManga.py
@@ -5,11 +5,6 @@ from PySide6.QtWidgets import QMenu
 class LibraryMangaMenu(QMenu):
     def __init__(self):
         super().__init__()
-        self.setStyleSheet("""
-            QMenu{background-color: rgb(45, 45, 45);color: rgb(255, 255, 255);}
-            QMenu::item{background-color: rgb(45, 45, 45);color: rgb(255, 255, 255);}
-            QMenu::item:selected{background-color: gray;}""")
-
         self.add_to_lib = QAction('Добавить в библиотеку')
         self.remove_from_lib = QAction('Удалить из библиотеки')
         self.open_in_browser = QAction('Открыть в браузере')
