c8f9183eedcc1234c1078e28f532ced9a03907e1
bzorn <v.klimenko.2137@gmail.com>
2022-12-22 22:17:05 +0300

    removed style_manager

--- nlightreader/utils/__init__.py
+++ nlightreader/utils/__init__.py
@@ -1,6 +1,5 @@
 from .catalog_manager import *
 from .database import Database
 from .file_manager import *
-from .style_manager import StyleManager
 from .text_formatter import TextFormatter
 from .utils import *
