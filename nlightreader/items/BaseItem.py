f8704a43a35848760eb08563fcc29a99c725f941
bzorn <v.klimenko.2137@gmail.com>
2022-12-26 17:07:21 +0300

    fixed items language

--- nlightreader/items/BaseItem.py
+++ nlightreader/items/BaseItem.py
@@ -1,3 +1,6 @@
+from PySide6.QtCore import QLocale
+
+
 class BaseItem:
     def __init__(self, item_id: str, name: str, russian: str):
         self.id = item_id
@@ -5,6 +8,7 @@ class BaseItem:
         self.russian = russian
 
     def get_name(self) -> str:
-        if self.russian:
-            return self.russian
-        return self.name
+        if QLocale().language() in (QLocale.Language.Russian, QLocale.Language.Ukrainian):
+            if self.russian:
+                return self.russian
+        return self.name.capitalize()
