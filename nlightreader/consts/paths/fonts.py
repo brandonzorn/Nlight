f0476ece4f31cd311140e2bb8a83d1795110ba88
bzorn <v.klimenko.2137@gmail.com>
2023-01-31 20:20:31 +0300

    added additional fonts

---
+++ nlightreader/consts/paths/fonts.py
@@ -0,0 +1,4 @@
+from nlightreader.consts.paths.paths import get_resource_path
+
+helvetica_light_path = get_resource_path("data/icons/lang/gb.svg")
+helvetica_regular_path = get_resource_path("data/icons/lang/ru.svg")
