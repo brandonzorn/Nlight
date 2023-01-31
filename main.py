1031533629e14cbbaab4b103d576a18deb1afcb2
bzorn <v.klimenko.2137@gmail.com>
2023-01-31 20:20:31 +0300

    added additional fonts

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import time
 
 import darkdetect
 from PySide6.QtCore import QSize, Qt, QTranslator, QLocale, QThreadPool
-from PySide6.QtGui import QIcon
+from PySide6.QtGui import QIcon, QFont
 from PySide6.QtWidgets import QApplication
 
 from nlightreader import ParentWindow
 from nlightreader.consts import app_icon_path, APP_VERSION, APP_NAME
+from nlightreader.consts.paths.fonts import helvetica_regular_path
 from nlightreader.utils import init_app_paths, get_locale_path, get_ui_style, Worker
 
 
@@ -41,6 +42,8 @@ if __name__ == '__main__':
     trans = QTranslator()
     trans.load(get_locale_path(QLocale().language()))
     app.installTranslator(trans)
+    font = QFont(helvetica_regular_path, 9)
+    app.setFont(font)
     app.setApplicationDisplayName(APP_NAME)
     app.setApplicationVersion(APP_VERSION)
     app.setWindowIcon(QIcon(app_icon_path))
