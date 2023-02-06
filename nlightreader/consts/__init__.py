6cbcf1cae9f1c03371710469aec4ae1c1a3777af
bzorn <v.klimenko.2137@gmail.com>
2023-02-06 11:46:47 +0300

    fix bugs

--- nlightreader/consts/__init__.py
+++ nlightreader/consts/__init__.py
@@ -1,7 +1,7 @@
 from .app import APP_NAME, APP_VERSION
-from .paths import TOKEN_PATH, APP_PATHS
 from .colors import ItemsColors
-from .items import DesuItems, ShikimoriItems
 from .files import Styles, Icons, LangIcons, Fonts, Translations
+from .items import DesuItems, ShikimoriItems
 from .lists import LibList, MangaKinds, lib_lists_en, parse_lib_list
+from .paths import TOKEN_PATH, APP_PATHS
 from .urls import *
