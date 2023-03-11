d90ac27b893df973e47cbbdb94563238fb78352d
bzorn <v.klimenko.2137@gmail.com>
2023-03-11 09:52:00 +0300

    moved cache to user dir

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+import os
 import sys
 import time
 
 import darkdetect
+import platformdirs
 from PySide6.QtCore import QSize, Qt, QTranslator, QLocale, QThreadPool
 from PySide6.QtGui import QIcon, QFont
 from PySide6.QtWidgets import QApplication
 
 from nlightreader import ParentWindow
-from nlightreader.consts import APP_VERSION, APP_NAME, APP_PATHS, Icons, Fonts
-from nlightreader.utils import init_app_paths, get_locale, get_ui_style, Worker
+from nlightreader.consts import APP_VERSION, APP_NAME, Icons, Fonts
+from nlightreader.utils import get_locale, get_ui_style, Worker
 
 
 class App(ParentWindow):
@@ -47,6 +49,6 @@ if __name__ == '__main__':
     app.setApplicationVersion(APP_VERSION)
     app.setWindowIcon(QIcon(Icons.App))
     app.setStyleSheet(get_ui_style(darkdetect.theme()))
-    init_app_paths(APP_PATHS)
+    os.makedirs(f'{platformdirs.user_data_dir()}/{APP_NAME}', exist_ok=True)
     a = App()
     sys.exit(app.exec())
