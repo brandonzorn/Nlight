eb905a87063d930be3e8c58e7ad06297925955bd
bzorn <v.klimenko.2137@gmail.com>
2023-03-14 09:06:01 +0300

    Fixed text formatting

--- nlightreader/utils/text_formatter.py
+++ nlightreader/utils/text_formatter.py
@@ -9,14 +9,14 @@ class TextFormatter:
         self._show_spoilers = show_spoilers
 
     def replace_spoilers(self):
-        for i, spoiler_text in re.findall(r"\[spoiler=(\w+)](.+?)\[/spoiler]", self._text):
+        for i, spoiler_text in re.findall(r"\[spoiler=(\w+)](.+?)\[/spoiler]", self._text, re.DOTALL):
             spoiler = spoiler_text
             if not self._show_spoilers:
                 spoiler = ""
             self._text = self._text.replace(
                 f"[spoiler={i}]{spoiler_text}[/spoiler]",
                 f'<span style=" color:#951a00;">{spoiler}</span>')
-        for spoiler_text in re.findall(r"\[spoiler](.+?)\[/spoiler]", self._text):
+        for spoiler_text in re.findall(r"\[spoiler](.+?)\[/spoiler]", self._text, re.DOTALL):
             spoiler = spoiler_text
             if not self._show_spoilers:
                 spoiler = ""
@@ -25,13 +25,17 @@ class TextFormatter:
                 f'<span style=" color:#951a00;">{spoiler}</span>')
 
     def replace_characters(self):
-        for ch_id, name in re.findall(r"\[character=(\w+)](.+?)\[/character]", self._text):
+        for ch_id, name in re.findall(r"\[character=(\w+)](.+?)\[/character]", self._text, re.DOTALL):
             self._text = self._text.replace(
                 f"[character={ch_id}]{name}[/character]",
                 f'<span style=" color:#177e00;">{name}</span>')
+        for ch_id, name in re.findall(r"\[character=(\w+) (.+?)]", self._text, re.DOTALL):
+            self._text = self._text.replace(
+                f"[character={ch_id} {name}]",
+                f'<span style=" color:#177e00;">{name}</span>')
 
     def replace_urls(self):
-        for url, url_text in re.findall(r"\[url=(.+?)](.+?)\[/url]", self._text):
+        for url, url_text in re.findall(r"\[url=(.+?)](.+?)\[/url]", self._text, re.DOTALL):
             self._text = self._text.replace(
                 f"[url={url}]{url_text}[/url]",
                 f'<a href="{url}">'
