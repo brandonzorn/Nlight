e6c5fa455a7c548be6cc6c0eaa37dbc4274b532e
bzorn <v.klimenko.2137@gmail.com>
2023-04-05 16:48:29 +0300

    /

--- nlightreader/consts/urls/urls.py
+++ nlightreader/consts/urls/urls.py
@@ -1,12 +1,16 @@
 URL_DESU = 'https://desu.me'
 URL_DESU_API = 'https://desu.me/manga/api'
+
 URL_SHIKIMORI = 'https://shikimori.one'
 URL_SHIKIMORI_API = 'https://shikimori.one/api'
 URL_SHIKIMORI_TOKEN = 'https://shikimori.one/oauth/token'
+
 URL_MANGA_DEX = 'https://mangadex.org'
 URL_MANGA_DEX_API = 'https://api.mangadex.org'
+
 URL_RULATE = 'https://tl.rulate.ru'
 URL_EROLATE = 'https://erolate.com'
+
 URL_RANOBEHUB = 'https://ranobehub.org'
 URL_RANOBEHUB_API = 'https://ranobehub.org/api'
 
