b03d13fcf67bb70000b76793d77225a118b0fa4b
bzorn <v.klimenko.2137@gmail.com>
2023-04-09 17:23:48 +0300

    added catalog Remanga(base support)

--- nlightreader/consts/urls/urls.py
+++ nlightreader/consts/urls/urls.py
@@ -14,6 +14,9 @@ URL_EROLATE = 'https://erolate.com'
 URL_RANOBEHUB = 'https://ranobehub.org'
 URL_RANOBEHUB_API = 'https://ranobehub.org/api'
 
+URL_REMANGA = "https://remanga.org"
+URL_REMANGA_API = "https://remanga.org/api"
+
 
 DEFAULT_HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0'}
 DESU_HEADERS = {'User-Agent': 'Nlight'}
