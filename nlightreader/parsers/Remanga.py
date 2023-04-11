466cdfa1f90ecc158f258b1a56887f64f1a12dd7
bzorn <v.klimenko.2137@gmail.com>
2023-04-11 18:27:35 +0300

    added chapters remanga

--- nlightreader/parsers/Remanga.py
+++ nlightreader/parsers/Remanga.py
@@ -1,5 +1,9 @@
+import json
+
+from bs4 import BeautifulSoup
+
 from nlightreader.consts import URL_REMANGA_API, URL_REMANGA
-from nlightreader.items import RequestForm, Manga, Chapter
+from nlightreader.items import RequestForm, Manga, Chapter, Image
 from nlightreader.parsers.Parser import Parser
 from nlightreader.utils.utils import get_html
 
@@ -40,7 +44,47 @@ class Remanga(Parser):
         return mangas
 
     def get_chapters(self, manga: Manga) -> list[Chapter]:
-        return []
+        url = f'{self.url_api}/titles/{manga.content_id}'
+        html = get_html(url, self.headers)
+        chapters = []
+        if html and html.status_code == 200 and html.json():
+            data = html.json().get('content')
+            branch_id = data.get('branches')[0].get('id')
+            page = 1
+            while True:
+                chapters_data = get_html(f'{self.url_api}/titles/chapters/?branch_id={branch_id}&count=40&page={page}')
+                if chapters_data and chapters_data.status_code == 200 and chapters_data.json():
+                    data = chapters_data.json().get('content')
+                    if not data:
+                        break
+                    for ch in data:
+                        if ch.get('is_paid'):
+                            continue
+                        chapter = Chapter(ch.get('id'), self.catalog_id,
+                                          str(ch.get('tome')), ch.get('chapter'), ch.get('name'), 'ru')
+                        chapters.append(chapter)
+                else:
+                    break
+                page += 1
+        return chapters
+
+    def get_images(self, manga: Manga, chapter: Chapter):
+        url = f'{URL_REMANGA}/manga/{manga.content_id}/{chapter.content_id}'
+        html = get_html(url, self.headers)
+        images = []
+        if html and html.status_code == 200:
+            soup = BeautifulSoup(html.text, "html.parser")
+            script = soup.find("script", {'id': "__NEXT_DATA__", 'type': "application/json"})
+            site_json = json.loads(script.text)
+            pages_data = site_json['props']['pageProps']['fallbackData']['chapter']['content']["pages"]
+            if pages_data:
+                for i in pages_data:
+                    i = i[0]
+                    images.append(Image(i.get("id"), i.get("page"), i.get("link")))
+        return images
+
+    def get_image(self, image: Image):
+        return get_html(image.img, headers={'Referer': f'{URL_REMANGA}/manga/'}).content
 
     def get_preview(self, manga: Manga):
         url = f'{self.url_api}/titles/{manga.content_id}'
