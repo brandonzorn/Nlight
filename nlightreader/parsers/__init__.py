35f1d06f5515dc2322a555a5d4264a928ce7d536
bzorn <v.klimenko.2137@gmail.com>
2023-05-18 10:15:29 +0300

    fixed imports

--- nlightreader/parsers/__init__.py
+++ nlightreader/parsers/__init__.py
@@ -2,6 +2,6 @@ from .Desu import Desu
 from .LocalLib import LocalLib
 from .MangaDex import MangaDex, MangaDexLib
 from .Ranobehub import Ranobehub
+from .Remanga import Remanga
 from .Rulate import Rulate, Erolate
 from .Shikimori import ShikimoriLib, ShikimoriManga, ShikimoriRanobe, ShikimoriBase
-from .Remanga import Remanga
