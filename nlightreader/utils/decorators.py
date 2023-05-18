da16f3a40123a7a7d720ac3df90ec7c4c351047e
bzorn <v.klimenko.2137@gmail.com>
2023-05-18 13:20:35 +0300

    docstrings

--- nlightreader/utils/decorators.py
+++ nlightreader/utils/decorators.py
@@ -3,6 +3,15 @@ from typing import Callable
 
 
 def singleton(cls):
+    """
+    A decorator that transforms a class into a singleton.
+
+    Parameters:
+        cls (type): The class to be transformed.
+
+    Returns:
+        callable: Wrapped class as a singleton.
+    """
     instance = [None]
 
     @wraps(cls)
@@ -14,6 +23,15 @@ def singleton(cls):
 
 
 def with_lock_thread(locker):
+    """
+    A decorator that acquires a lock before executing a function in a threaded environment.
+
+    Parameters:
+        locker (threading.Lock): The lock object to acquire.
+
+    Returns:
+        callable: Decorator that acquires the lock before executing wrapped function.
+    """
     def decorator(func: Callable):
 
         @wraps(func)
