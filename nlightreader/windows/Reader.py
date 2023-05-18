5e32b4f4c7d43b71cb6c224d6acfc9b0ba286670
bzorn <v.klimenko.2137@gmail.com>
2023-05-18 10:16:54 +0300

    fixed threads

--- nlightreader/windows/Reader.py
+++ nlightreader/windows/Reader.py
@@ -7,7 +7,7 @@ from PySide6.QtWidgets import QMainWindow, QListWidgetItem
 from data.ui.windows.reader import Ui_ReaderWindow
 from nlightreader.consts import ItemsColors
 from nlightreader.items import Manga, Chapter, Image, HistoryNote
-from nlightreader.utils import Database, get_catalog, FileManager, translate, Worker, get_language_icon
+from nlightreader.utils import Database, get_catalog, FileManager, translate, get_language_icon, Thread
 
 
 class ReaderWindow(QMainWindow):
@@ -27,6 +27,8 @@ class ReaderWindow(QMainWindow):
         self.ui.text_size_slider.valueChanged.connect(self.update_text_size)
 
         self.ui.items_list.doubleClicked.connect(self.change_chapter)
+
+        self._set_image_thread = Thread(target=self.get_content, callback=self.update_image)
 
         self.db: Database = Database()
         self.manga = None
@@ -121,8 +123,8 @@ class ReaderWindow(QMainWindow):
             self.update_page()
 
     def update_page(self):
-        self.attach_image()
         self.ui.page_label.setText(f"{translate('Other', 'Page')} {self.cur_page} / {self.max_page}")
+        self.attach_image()
 
     @Slot()
     def turn_chapter_next(self):
@@ -149,43 +151,43 @@ class ReaderWindow(QMainWindow):
 
     def reset_reader_area(self):
         self.ui.img.clear()
+        self.ui.text.clear()
         self.ui.scrollArea.verticalScrollBar().setValue(0)
         self.ui.scrollArea.horizontalScrollBar().setValue(0)
         self.ui.scrollAreaWidgetContents.resize(0, 0)
 
     def attach_image(self):
+        self._set_image_thread.terminate()
+        self._set_image_thread.wait()
         self.reset_reader_area()
         self.cur_image_pixmap = None
         if not self.images:
             return
-        Worker(target=self.set_image, callback=self.update_image).start()
+        self._set_image_thread.start()
 
-    def set_image(self):
+    def get_content(self):
         page = self.cur_page
         chapter = self.cur_chapter
 
-        def get_image():
-            if not FileManager.check_image_exists(
-                    self.manga, self.chapters[chapter - 1], self.images[page - 1], self.catalog):
-                time.sleep(0.25)
-                if page != self.cur_page or chapter != self.cur_chapter:
-                    return
-                self.ui.img.setText(translate('Other', 'Page is loading'))
-            if self.manga.kind == 'ranobe':
-                self.cur_image_pixmap = FileManager.get_chapter_text_file(
+        if not FileManager.check_image_exists(
+                self.manga, self.chapters[chapter - 1], self.images[page - 1], self.catalog):
+            time.sleep(0.25)
+            if page != self.cur_page or chapter != self.cur_chapter:
+                return
+            self.ui.img.setText(translate('Other', 'Page is loading'))
+        if self.manga.kind == 'ranobe':
+            self.cur_image_pixmap = FileManager.get_chapter_text_file(
+                self.manga, self.chapters[chapter - 1], self.images[page - 1], self.catalog)
+        else:
+            if not self.cur_image_pixmap:
+                self.cur_image_pixmap = FileManager.get_image_file(
                     self.manga, self.chapters[chapter - 1], self.images[page - 1], self.catalog)
-            else:
-                if not self.cur_image_pixmap:
-                    self.cur_image_pixmap = FileManager.get_image_file(
-                        self.manga, self.chapters[chapter - 1], self.images[page - 1], self.catalog)
-        get_image()
-        assert page == self.cur_page or chapter == self.cur_chapter
 
     def update_image(self):
         if self.manga.kind == 'ranobe':
             self.ui.text.setHtml(self.cur_image_pixmap)
         else:
-            pixmap = self.resize_pixmap(self.cur_image_pixmap)
+            pixmap = self._resize_pixmap(self.cur_image_pixmap)
             self.ui.img.setPixmap(pixmap)
 
     @Slot()
@@ -194,7 +196,7 @@ class ReaderWindow(QMainWindow):
         font.setPointSize(self.ui.text_size_slider.value())
         self.ui.text.setFont(font)
 
-    def resize_pixmap(self, pixmap: QPixmap) -> QPixmap:
+    def _resize_pixmap(self, pixmap: QPixmap) -> QPixmap:
         if pixmap is None or pixmap.isNull():
             return QPixmap()
         if 0.5 < pixmap.width() / pixmap.height() < 2:
