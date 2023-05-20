4c5b68d454645b492092b28b62c42cbc0a44ba46
bzorn <v.klimenko.2137@gmail.com>
2023-05-20 10:10:15 +0300

    updated docstring

--- nlightreader/utils/utils.py
+++ nlightreader/utils/utils.py
@@ -16,6 +16,7 @@ def get_html(url: str, headers=None, params=None, json=None, cookies=None, conte
         :param url: The URL to request.
         :param headers: Optional dictionary of request headers.
         :param params: Optional dictionary of query parameters.
+        :param json: Optional dictionary of json to include in the request.
         :param cookies: Optional dictionary of cookies to include in the request.
         :param content_type: Optional string indicating the expected content type of the response ('content' or 'json').
         :return: If content_type is 'content', returns the raw response content (bytes).
@@ -26,7 +27,7 @@ def get_html(url: str, headers=None, params=None, json=None, cookies=None, conte
     if headers is None:
         headers = DEFAULT_HEADERS
     if "test" in QApplication.arguments():
-        return None
+        return
     try:
         response = requests.get(url, headers=headers, params=params, json=json, cookies=cookies)
         response.raise_for_status()
@@ -42,7 +43,8 @@ def get_html(url: str, headers=None, params=None, json=None, cookies=None, conte
         print(f"  Headers: {headers}")
         print(f"  Params: {params}")
         print(f"  Cookies: {cookies}")
-        return None
+        print(f"  Json: {json}")
+        return
 
 
 def get_language_icon(language: str) -> str:
