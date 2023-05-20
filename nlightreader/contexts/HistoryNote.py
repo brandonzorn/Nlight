34a90379dd9c4ef283044dfcc971ebdc9bab4648
bzorn <v.klimenko.2137@gmail.com>
2023-05-20 10:09:21 +0300

    updated QMenu actions

--- nlightreader/contexts/HistoryNote.py
+++ nlightreader/contexts/HistoryNote.py
@@ -11,9 +11,19 @@ class HistoryNoteMenu(QMenu):
         self.remove_all = QAction(translate("Menu", "Remove all"))
 
     def set_mode(self, mode: int):
-        match mode:
-            case 0:
-                self.addAction(self.remove_all)
-                self.addAction(self.set_as_read)
-            case 1:
-                self.addAction(self.remove_all)
+        """
+        Sets the mode of this object and adds the appropriate actions based on the mode.
+
+        Args:
+            mode (int): The mode to set. Valid values are 0, 1.
+
+        Raises:
+            ValueError: If an invalid mode is provided.
+        """
+        actions = {
+            0: [self.remove_all, self.set_as_read],
+            1: [self.remove_all]
+        }
+        if mode not in actions:
+            raise ValueError("Invalid mode: must be 0 or 1")
+        self.addActions(actions[mode])
--- nlightreader/contexts/LibraryManga.py
+++ nlightreader/contexts/LibraryManga.py
@@ -1,5 +1,5 @@
 from PySide6.QtGui import QAction
-from PySide6.QtWidgets import QMenu, QApplication
+from PySide6.QtWidgets import QMenu
 
 from nlightreader.utils import translate
 
@@ -14,11 +14,20 @@ class LibraryMangaMenu(QMenu):
         self.open_local_files = QAction(translate("Menu", "Open local files"))
 
     def set_mode(self, mode: int):
-        self.addAction(self.open_in_browser)
-        match mode:
-            case 0:
-                self.addAction(self.add_to_lib)
-            case 1:
-                self.addAction(self.remove_from_lib)
-        self.addAction(self.open_local_files)
-        self.addAction(self.remove_files)
+        """
+        Sets the mode of this object and adds the appropriate actions based on the mode.
+
+        Args:
+            mode: The mode to set. Valid values are 0, 1, 2.
+
+        Raises:
+            ValueError: If an invalid mode is provided.
+        """
+        actions = {
+            0: [self.open_in_browser, self.add_to_lib, self.open_local_files, self.remove_files],
+            1: [self.open_in_browser, self.remove_from_lib, self.open_local_files, self.remove_files],
+            2: [self.open_in_browser, self.open_local_files, self.remove_files]
+        }
+        if mode not in actions:
+            raise ValueError("Invalid mode: must be 0, 1 or 2")
+        self.addActions(actions.get(mode, []))
