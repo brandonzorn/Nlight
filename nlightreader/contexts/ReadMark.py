01af63f0c2cb529cecdda3bdaca34ad8de49cecb
bzorn <v.klimenko.2137@gmail.com>
2023-05-20 10:09:21 +0300

    updated QMenu actions

--- nlightreader/contexts/ReadMark.py
+++ nlightreader/contexts/ReadMark.py
@@ -12,14 +12,20 @@ class ReadMarkMenu(QMenu):
         self.remove_read_state = QAction(translate("Menu", "Remove read mark"))
 
     def set_mode(self, mode: int):
-        match mode:
-            case 0:
-                self.addAction(self.set_as_read)
-                self.addAction(self.set_as_read_all)
-            case 1:
-                self.addAction(self.remove_read_state)
-                self.addAction(self.set_as_read_all)
-            case 2:
-                self.addAction(self.set_as_read)
-                self.addAction(self.remove_read_state)
-                self.addAction(self.set_as_read_all)
+        """
+        Sets the mode of this object and adds the appropriate actions based on the mode.
+
+        Args:
+            mode (int): The mode to set. Valid values are 0, 1, 2.
+
+        Raises:
+            ValueError: If an invalid mode is provided.
+        """
+        actions = {
+            0: [self.set_as_read, self.set_as_read_all],
+            1: [self.remove_read_state, self.set_as_read_all],
+            2: [self.set_as_read, self.remove_read_state, self.set_as_read_all]
+        }
+        if mode not in actions:
+            raise ValueError("Invalid mode: must be 0, 1 or 2")
+        self.addActions(actions.get(mode, []))
