861833aa9f09fdeacbcde6044cf12b3a91a6b03d
bzorn <v.klimenko.2137@gmail.com>
2023-06-18 21:05:59 +0300

    added NHentai catalog

--- nlightreader/parsers/__init__.py
+++ nlightreader/parsers/__init__.py
@@ -1,4 +1,5 @@
 from .LocalLib import LocalLib
 from .Shikimori import ShikimoriLib, ShikimoriManga, ShikimoriRanobe, ShikimoriBase
+from .hentai_manga import NHentai
 from .manga import Desu, MangaDex, MangaDexLib, Remanga
 from .ranobe import Rulate, Erolate, Ranobehub
