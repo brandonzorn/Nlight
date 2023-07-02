42019bb4a01b29463392f661cdbd605ac2f3432c
bzorn <v.klimenko.2137@gmail.com>
2023-07-02 10:13:28 +0300

    fixed reader ui

--- nlightreader/widgets/NlightContainers/image_area.py
+++ nlightreader/widgets/NlightContainers/image_area.py
@@ -1,33 +1,15 @@
 from PySide6.QtCore import Qt, QSize
 from PySide6.QtGui import QPixmap
-from PySide6.QtWidgets import QScrollArea, QLabel, QWidget, QVBoxLayout
+from PySide6.QtWidgets import QWidget
+
+from data.ui.containers.image_area import Ui_Form
 
 
-class ImageArea(QScrollArea):
+class ImageArea(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWidgetResizable(True)
-        self.setFocusPolicy(Qt.NoFocus)
-
-        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-
-        self._scrollAreaWidgetContents = QWidget()
-        self._scrollAreaWidgetContents.setAutoFillBackground(True)
-        self._scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-
-        self._scroll_layout = QVBoxLayout(self._scrollAreaWidgetContents)
-
-        self._scroll_layout.setSpacing(6)
-        self._scroll_layout.setContentsMargins(0, 0, 0, 0)
-
-        self._img_lbl = QLabel(self._scrollAreaWidgetContents)
-        self._img_lbl.setAlignment(Qt.AlignCenter)
-        self._img_lbl.setTextInteractionFlags(Qt.NoTextInteraction)
-
-        self._scroll_layout.addWidget(self._img_lbl)
-        self.setWidget(self._scrollAreaWidgetContents)
-
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
         self._image_pixmap = None
 
     def install(self, parent):
@@ -37,40 +19,39 @@ class ImageArea(QScrollArea):
         super().resizeEvent(arg__1)
         if (self._image_pixmap is None) or (arg__1.oldSize() == arg__1.size()):
             return
-        self._img_lbl.clear()
-
-        view_w = self.viewport().width()
-        self._img_lbl.setFixedWidth(view_w)
-        self._scrollAreaWidgetContents.setFixedWidth(view_w)
-
-        self._scrollAreaWidgetContents.resize(self.viewport().size())
-
+        view_w = self.ui.scrollArea.viewport().width()
+        self.ui.img_lbl.setFixedWidth(view_w)
+        self.ui.scrollAreaWidgetContents.setFixedWidth(view_w)
+        self.ui.scrollAreaWidgetContents.resize(self.ui.scrollArea.viewport().size())
         self.update_image()
 
     def reset_area(self):
-        self._img_lbl.clear()
-        self.verticalScrollBar().setValue(0)
-        self.horizontalScrollBar().setValue(0)
-        self._scrollAreaWidgetContents.resize(self.viewport().size())
+        self.ui.img_lbl.clear()
+        self.ui.scrollArea.verticalScrollBar().setValue(0)
+        self.ui.scrollArea.horizontalScrollBar().setValue(0)
+        view_w = self.ui.scrollArea.viewport().width()
+        self.ui.img_lbl.setFixedWidth(view_w)
+        self.ui.scrollAreaWidgetContents.setFixedWidth(view_w)
+        self.ui.scrollAreaWidgetContents.resize(self.ui.scrollArea.viewport().size())
 
     def _resize_pixmap(self, pixmap: QPixmap) -> QPixmap:
         if pixmap is None or pixmap.isNull():
             return QPixmap()
         if 0.5 < pixmap.width() / pixmap.height() < 2:
-            pixmap = pixmap.scaled(self.viewport().size(), Qt.AspectRatioMode.KeepAspectRatio,
+            pixmap = pixmap.scaled(self.ui.scrollArea.viewport().size(), Qt.AspectRatioMode.KeepAspectRatio,
                                    Qt.TransformationMode.SmoothTransformation)
-            self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+            self.ui.scrollArea.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         else:
-            w = self.viewport().width()
+            w = self.ui.scrollArea.viewport().width()
             h = pixmap.height()
             pixmap = pixmap.scaled(QSize(w, h), Qt.AspectRatioMode.KeepAspectRatio,
                                    Qt.TransformationMode.SmoothTransformation)
-            self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+            self.ui.scrollArea.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
         return pixmap
 
     def update_image(self):
         pixmap = self._resize_pixmap(self._image_pixmap)
-        self._img_lbl.setPixmap(pixmap)
+        self.ui.img_lbl.setPixmap(pixmap)
 
     def set_image(self, img_pixmap: QPixmap):
         self._image_pixmap = img_pixmap
@@ -78,4 +59,4 @@ class ImageArea(QScrollArea):
         self.update_image()
 
     def set_text(self, text: str):
-        self._img_lbl.setText(text)
+        self.ui.img_lbl.setText(text)
