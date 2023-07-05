66d9f84cd4929460fa135198b10fb17b270d9273
bzorn <v.klimenko.2137@gmail.com>
2023-07-05 20:23:57 +0300

    added qss color binds

--- nlightreader/consts/__init__.py
+++ nlightreader/consts/__init__.py
@@ -1,5 +1,5 @@
 from .app import APP_NAME, APP_VERSION
-from .colors import ItemsColors
+from .colors import ItemsColors, StyleColors
 from .files import Styles, Icons, LangIcons, Translations
 from .lists import LibList, MangaKinds, lib_lists_en, parse_lib_list
 from .paths import TOKEN_PATH
