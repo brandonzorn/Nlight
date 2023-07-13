746021021949d38c44f66f456bff920a3c8ff219
bzorn <v.klimenko.2137@gmail.com>
2023-07-13 13:20:49 +0300

    Updated description

--- make_version_file.py
+++ make_version_file.py
@@ -6,7 +6,7 @@ pyinstaller_versionfile.create_versionfile(
     output_file="pkg_res/version_info.txt",
     version=APP_VERSION,
     company_name="brandonzorn",
-    file_description="Open source manga and ranobe reading application",
+    file_description=f"{APP_NAME} - Manga Reader",
     internal_name=APP_NAME,
     legal_copyright="Copyright (c) 2022 brandonzorn",
     original_filename=f"{APP_NAME}.exe",
