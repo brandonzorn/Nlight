5437e179d9cb52dd552d03bc81abd24bd744ad1a
bzorn <v.klimenko.2137@gmail.com>
2023-08-26 13:30:58 +0300

    Update makepkg.sh

--- makepkg.sh
+++ makepkg.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
+
+DEB_FILE = Nlight.deb
+RPM_FILE = Nlight.rpm
+TAR_FILE = Nlight.tar.zst
+
 [ -e package ] && rm -r package
 [ -e dist ] && rm -r dist
-[ -e Nlight.deb ] && rm -r Nlight.deb
+[ -e $RPM_FILE ] && rm -r $RPM_FILE
+[ -e $TAR_FILE ] && rm -r $TAR_FILE
+[ -e $DEB_FILE ] && rm -r $DEB_FILE
+
 
 . venv/bin/activate
-pyinstaller --noconfirm --windowed --name "Nlight" "main.py"
+pyinstaller --noconfirm --windowed --name "Nlight" --key "6w9z$C&F)J@NcQfTjWnZr4u7x!A%D*" "main.py"
 
 mkdir -p package/opt
 mkdir -p package/usr/share/applications
@@ -19,4 +27,18 @@ find package/opt/Nlight -type d -exec chmod 755 -- {} +
 find package/usr/share -type f -exec chmod 644 -- {} +
 chmod +x package/opt/Nlight/Nlight
 
-fpm -C package -s dir -t deb -n "nlight" -v 1.9.6 --license mit --depends libxcb-cursor0 --description "Open source manga and ranobe reading application" --url "https://github.com/brandonzorn/Nlight" --architecture native --maintainer "brandonzorn <v.klimenko.2137@gmail.com>" -p dist/Nlight.deb
+
+make_package()
+{
+  FPM_EXECUTABLE_PATH = /home/bzorn/.local/share/gem/ruby/3.0.0/bin/fpm
+  DESCRIPTION = "Open source manga and ranobe reading application"
+  URL = "https://github.com/brandonzorn/Nlight"
+  MAINTAINER = "brandonzorn <v.klimenko.2137@gmail.com>"
+  PACKAGE_TYPE = $1
+  FILE_NAME = $2
+  $FPM_EXECUTABLE_PATH -C package -s dir -t $PACKAGE_TYPE -n "nlight" -v 1.9.9 --license mit --description $DESCRIPTION  --url $URL --architecture native --maintainer @MAINTAINER -p dist/$FILE_NAME
+}
+
+make_package deb $DEB_FILE
+make_package rpm $RPM_FILE
+make_package pacman $TAR_FILE
