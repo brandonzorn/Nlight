36d82ce53f4f13e5055ee971b56cf634997e6e35
bzorn <v.klimenko.2137@gmail.com>
2023-12-11 20:27:47 +0300

    added new MangaDex auth and reformatted code

--- nlightreader/windows/Reader.py
+++ nlightreader/windows/Reader.py
@@ -48,7 +48,7 @@ class ReaderWindow(QMainWindow):
         self.ui.chapters_frame.hide()
         self.manga = manga
         self.setWindowTitle(self.manga.name)
-        if self.manga.kind == 'ranobe':
+        if self.manga.kind == "ranobe":
             self.text_container.install(self.ui.reader_layout)
         else:
             self.image_container.install(self.ui.reader_layout)
@@ -161,8 +161,8 @@ class ReaderWindow(QMainWindow):
             time.sleep(0.25)
             if page != self.cur_page or chapter != self.cur_chapter:
                 return
-            self.image_container.set_text(translate('Other', 'Page is loading'))
-        if self.manga.kind == 'ranobe':
+            self.image_container.set_text(translate("Other", "Page is loading"))
+        if self.manga.kind == "ranobe":
             self.cur_image_pixmap = FileManager.get_chapter_text_file(
                 self.manga, self.chapters[chapter - 1], self.images[page - 1], self.catalog)
         else:
@@ -171,7 +171,7 @@ class ReaderWindow(QMainWindow):
                     self.manga, self.chapters[chapter - 1], self.images[page - 1], self.catalog)
 
     def update_image(self):
-        if self.manga.kind == 'ranobe':
+        if self.manga.kind == "ranobe":
             self.text_container.set_html(self.cur_image_pixmap)
         else:
             self.image_container.set_image(self.cur_image_pixmap)
