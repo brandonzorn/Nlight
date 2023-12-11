279e1cf4d9c3ee87ace93ce71b1154730b3f9fdf
bzorn <v.klimenko.2137@gmail.com>
2023-12-11 20:22:09 +0300

    reformat contexts

--- nlightreader/contexts/LibraryManga.py
+++ nlightreader/contexts/LibraryManga.py
@@ -7,11 +7,13 @@ from nlightreader.utils import translate
 class LibraryMangaMenu(QMenu):
     def __init__(self):
         super().__init__()
-        self.add_to_lib = QAction(translate('Menu', 'Add to Library'))
-        self.remove_from_lib = QAction(translate('Menu', 'Remove from library'))
-        self.open_in_browser = QAction(translate('Menu', 'Open in browser'))
-        self.remove_files = QAction(translate('Menu', 'Clear local files'))
-        self.open_local_files = QAction(translate('Menu', 'Open local files'))
+        self.add_to_lib = QAction(translate("Menu", "Add to Library"))
+        self.remove_from_lib = QAction(
+            translate("Menu", "Remove from library")
+        )
+        self.open_in_browser = QAction(translate("Menu", "Open in browser"))
+        self.remove_files = QAction(translate("Menu", "Clear local files"))
+        self.open_local_files = QAction(translate("Menu", "Open local files"))
 
     def set_mode(self, mode: int):
         """
@@ -43,5 +45,5 @@ class LibraryMangaMenu(QMenu):
             ],
         }
         if mode not in actions:
-            raise ValueError('Invalid mode: must be 0, 1 or 2')
+            raise ValueError("Invalid mode: must be 0, 1 or 2")
         self.addActions(actions.get(mode, []))
