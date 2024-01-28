b2cbb9d6b851a7b4f4d663d46c29cc760488c11d
bzorn <v.klimenko.2137@gmail.com>
2024-01-28 16:47:31 +0300

    reformatted code

--- nlightreader/consts/__init__.py
+++ nlightreader/consts/__init__.py
@@ -1,6 +1,6 @@
 from .app import APP_NAME, APP_VERSION
 from .colors import ItemsColors, StyleColors
-from .files import Styles, Icons, LangIcons, Translations
 from .enums import Nl, lib_lists_en, parse_lib_list
+from .files import Styles, Icons, LangIcons, Translations
 from .paths import TOKEN_PATH
 from .urls import *
--- nlightreader/parsers/manga/__init__.py
+++ nlightreader/parsers/manga/__init__.py
@@ -1,4 +1,4 @@
+from .Lib import SlashLib, MangaLib
 from .desu_manga import Desu
 from .mangadex_manga import MangaDex, MangaDexLib
 from .remanga_manga import Remanga
-from .Lib import SlashLib, MangaLib
