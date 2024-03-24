86d9863dc68dda97e58556c884d9ec7607fa9771
bzorn <v.klimenko.2137@gmail.com>
2024-03-24 09:50:45 +0300

    code refactoring

--- nlightreader/utils/threads.py
+++ nlightreader/utils/threads.py
@@ -14,40 +14,38 @@ class Signals(QObject):
     finished = Signal()
 
 
-class Worker(QRunnable):
-    def __init__(self, target: Callable, args=(), kwargs=None, *, callback=None, locker=None):
-        """
-        Initializes a new `Runnable` instance.
-
-        :param target: A callable object representing the target function to run in the thread.
-        :param args: An optional tuple or list containing the arguments to pass to the target function.
-            Defaults to an empty tuple.
-        :param kwargs: An optional dictionary containing keyword arguments to pass to the target function.
-            Defaults to an empty dictionary.
-        :param callback: An optional callable object to invoke when the thread finishes running. Defaults to None.
-        :param locker: An optional QMutex object to lock the thread during execution. Defaults to None.
-        """
-        super(Worker, self).__init__()
+class NlThread:
+    def __init__(self, target: Callable, args=(), kwargs=None, *, callback=None):
+        super().__init__()
         if kwargs is None:
             kwargs = {}
         self._target = target
         self._args = args
         self._kwargs = kwargs
-        self._locker = locker
         self.signals = Signals()
         if callback:
             self.signals.finished.connect(callback)
 
     @Slot()
     def run(self):
-        if self._locker:
-            while not self._locker.tryLock():
-                pass
-            self._target(*self._args, **self._kwargs)
-            self._locker.unlock()
-        else:
-            self._target(*self._args, **self._kwargs)
+        self._target(*self._args, **self._kwargs)
         self.signals.finished.emit()
+
+
+class Worker(NlThread, QRunnable):
+    """
+    Initializes a new `Runnable` instance.
+
+    :param target: A callable object representing the target function to run in the thread.
+    :param args: An optional tuple or list containing the arguments to pass to the target function.
+    Defaults to an empty tuple.
+    :param kwargs: An optional dictionary containing keyword arguments to pass to the target function.
+    Defaults to an empty dictionary.
+    :param callback: An optional callable object to invoke when the thread finishes running. Defaults to None.
+    """
+
+    def __init__(self, target: Callable, args=(), kwargs=None, *, callback=None):
+        super().__init__(target, args, kwargs, callback=callback)
 
     def start(self, pool=None):
         if pool is None:
@@ -57,37 +55,17 @@ class Worker(QRunnable):
         pool.start(self)
 
 
-class Thread(QThread):
-    def __init__(self, target: Callable, args=(), kwargs=None, *, callback=None, locker=None):
-        """
-        Initializes a new `Thread` instance.
+class Thread(NlThread, QThread):
+    """
+    Initializes a new `Thread` instance.
 
-        :param target: A callable object representing the target function to run in the thread.
-        :param args: An optional tuple or list containing the arguments to pass to the target function.
-            Defaults to an empty tuple.
-        :param kwargs: An optional dictionary containing keyword arguments to pass to the target function.
-            Defaults to an empty dictionary.
-        :param callback: An optional callable object to invoke when the thread finishes running. Defaults to None.
-        :param locker: An optional QMutex object to lock the thread during execution. Defaults to None.
-        """
-        super().__init__()
-        if kwargs is None:
-            kwargs = {}
-        self._target = target
-        self._args = args
-        self._kwargs = kwargs
-        self._locker = locker
-        self.signals = Signals()
-        if callback:
-            self.signals.finished.connect(callback)
+    :param target: A callable object representing the target function to run in the thread.
+    :param args: An optional tuple or list containing the arguments to pass to the target function.
+    Defaults to an empty tuple.
+    :param kwargs: An optional dictionary containing keyword arguments to pass to the target function.
+    Defaults to an empty dictionary.
+    :param callback: An optional callable object to invoke when the thread finishes running. Defaults to None.
+    """
 
-    @Slot()
-    def run(self):
-        if self._locker:
-            while not self._locker.tryLock():
-                pass
-            self._target(*self._args, **self._kwargs)
-            self._locker.unlock()
-        else:
-            self._target(*self._args, **self._kwargs)
-        self.signals.finished.emit()
+    def __init__(self, target: Callable, args=(), kwargs=None, *, callback=None):
+        super().__init__(target, args, kwargs, callback=callback)
