08d72f73ef816f8bdc854f2afd252a04e2c18a59
bzorn <v.klimenko.2137@gmail.com>
2024-03-27 22:26:27 +0300

    updated image scroll area

--- data/ui/containers/image_area.py
+++ data/ui/containers/image_area.py
@@ -3,76 +3,53 @@
 ################################################################################
 ## Form generated from reading UI file 'image_area.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.4.3
+## Created by: Qt User Interface Compiler version 6.6.2
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (
-    QCoreApplication,
-    QDate,
-    QDateTime,
-    QLocale,
-    QMetaObject,
-    QObject,
-    QPoint,
-    QRect,
-    QSize,
-    QTime,
-    QUrl,
-    Qt,
-)
-from PySide6.QtGui import (
-    QBrush,
-    QColor,
-    QConicalGradient,
-    QCursor,
-    QFont,
-    QFontDatabase,
-    QGradient,
-    QIcon,
-    QImage,
-    QKeySequence,
-    QLinearGradient,
-    QPainter,
-    QPalette,
-    QPixmap,
-    QRadialGradient,
-    QTransform,
-)
-from PySide6.QtWidgets import QApplication, QFrame, QHBoxLayout, QLabel, QScrollArea, QSizePolicy, QVBoxLayout, QWidget
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QLabel,
+    QSizePolicy, QVBoxLayout, QWidget)
 
+from qfluentwidgets import ScrollArea
 
 class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
-            Form.setObjectName("Form")
+            Form.setObjectName(u"Form")
         Form.resize(674, 512)
         self.verticalLayout = QVBoxLayout(Form)
-        self.verticalLayout.setObjectName("verticalLayout")
+        self.verticalLayout.setObjectName(u"verticalLayout")
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.frame = QFrame(Form)
-        self.frame.setObjectName("frame")
+        self.frame.setObjectName(u"frame")
         self.frame.setFrameShape(QFrame.StyledPanel)
         self.frame.setFrameShadow(QFrame.Raised)
         self.horizontalLayout = QHBoxLayout(self.frame)
-        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalLayout.setContentsMargins(9, 9, 9, 9)
-        self.scrollArea = QScrollArea(self.frame)
-        self.scrollArea.setObjectName("scrollArea")
+        self.scrollArea = ScrollArea(self.frame)
+        self.scrollArea.setObjectName(u"scrollArea")
         self.scrollArea.setFocusPolicy(Qt.NoFocus)
         self.scrollArea.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.scrollArea.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.scrollArea.setWidgetResizable(True)
         self.scrollAreaWidgetContents = QWidget()
-        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
         self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 652, 490))
         self.scroll_layout = QVBoxLayout(self.scrollAreaWidgetContents)
-        self.scroll_layout.setObjectName("scroll_layout")
+        self.scroll_layout.setObjectName(u"scroll_layout")
         self.scroll_layout.setContentsMargins(0, 0, 0, 0)
         self.img_lbl = QLabel(self.scrollAreaWidgetContents)
-        self.img_lbl.setObjectName("img_lbl")
-        self.img_lbl.setAlignment(Qt.AlignHCenter | Qt.AlignTop)
+        self.img_lbl.setObjectName(u"img_lbl")
+        self.img_lbl.setAlignment(Qt.AlignHCenter|Qt.AlignTop)
         self.img_lbl.setTextInteractionFlags(Qt.NoTextInteraction)
 
         self.scroll_layout.addWidget(self.img_lbl)
@@ -81,16 +58,17 @@ class Ui_Form(object):
 
         self.horizontalLayout.addWidget(self.scrollArea)
 
+
         self.verticalLayout.addWidget(self.frame)
+
 
         self.retranslateUi(Form)
 
         QMetaObject.connectSlotsByName(Form)
-
     # setupUi
 
     def retranslateUi(self, Form):
-        Form.setWindowTitle(QCoreApplication.translate("Form", "Form", None))
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
         self.img_lbl.setText("")
-
     # retranslateUi
+
