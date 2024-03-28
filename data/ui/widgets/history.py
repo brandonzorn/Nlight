99a86239e16246877298dee66f82fc3935f54ffc
bzorn <v.klimenko.2137@gmail.com>
2024-03-28 17:20:35 +0300

    replaced frames with cards

--- data/ui/widgets/history.py
+++ data/ui/widgets/history.py
@@ -1,111 +1,79 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'history.ui'
+## Form generated from reading UI file 'historyyuCQTK.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.4.3
+## Created by: Qt User Interface Compiler version 6.6.2
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (
-    QCoreApplication,
-    QDate,
-    QDateTime,
-    QLocale,
-    QMetaObject,
-    QObject,
-    QPoint,
-    QRect,
-    QSize,
-    QTime,
-    QUrl,
-    Qt,
-)
-from PySide6.QtGui import (
-    QBrush,
-    QColor,
-    QConicalGradient,
-    QCursor,
-    QFont,
-    QFontDatabase,
-    QGradient,
-    QIcon,
-    QImage,
-    QKeySequence,
-    QLinearGradient,
-    QPainter,
-    QPalette,
-    QPixmap,
-    QRadialGradient,
-    QTransform,
-)
-from PySide6.QtWidgets import (
-    QApplication,
-    QFrame,
-    QHBoxLayout,
-    QHeaderView,
-    QSizePolicy,
-    QSpacerItem,
-    QTreeWidgetItem,
-    QVBoxLayout,
-    QWidget,
-)
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QHeaderView,
+    QSizePolicy, QSpacerItem, QTreeWidgetItem, QVBoxLayout,
+    QWidget)
 
-from qfluentwidgets import ToolButton, TreeWidget
-
+from qfluentwidgets import (CardWidget, SimpleCardWidget, ToolButton, TreeWidget)
 
 class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
-            Form.setObjectName("Form")
+            Form.setObjectName(u"Form")
         Form.resize(767, 391)
         Form.setLocale(QLocale(QLocale.English, QLocale.UnitedStates))
         self.horizontalLayout = QHBoxLayout(Form)
-        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.items_frame = QFrame(Form)
-        self.items_frame.setObjectName("items_frame")
+        self.items_frame = SimpleCardWidget(Form)
+        self.items_frame.setObjectName(u"items_frame")
         self.items_frame.setFrameShape(QFrame.StyledPanel)
         self.items_frame.setFrameShadow(QFrame.Raised)
         self.verticalLayout_2 = QVBoxLayout(self.items_frame)
-        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.items_tree = TreeWidget(self.items_frame)
         __qtreewidgetitem = QTreeWidgetItem()
-        __qtreewidgetitem.setText(0, "1")
+        __qtreewidgetitem.setText(0, u"1");
         self.items_tree.setHeaderItem(__qtreewidgetitem)
-        self.items_tree.setObjectName("items_tree")
+        self.items_tree.setObjectName(u"items_tree")
         self.items_tree.setContextMenuPolicy(Qt.CustomContextMenu)
         self.items_tree.header().setVisible(False)
 
         self.verticalLayout_2.addWidget(self.items_tree)
 
+
         self.horizontalLayout.addWidget(self.items_frame)
 
-        self.frame = QFrame(Form)
-        self.frame.setObjectName("frame")
+        self.frame = SimpleCardWidget(Form)
+        self.frame.setObjectName(u"frame")
         self.frame.setFrameShape(QFrame.StyledPanel)
         self.frame.setFrameShadow(QFrame.Raised)
         self.verticalLayout = QVBoxLayout(self.frame)
-        self.verticalLayout.setObjectName("verticalLayout")
+        self.verticalLayout.setObjectName(u"verticalLayout")
         self.delete_btn = ToolButton(self.frame)
-        self.delete_btn.setObjectName("delete_btn")
+        self.delete_btn.setObjectName(u"delete_btn")
 
         self.verticalLayout.addWidget(self.delete_btn)
 
-        self.verticalSpacer = QSpacerItem(20, 231, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        self.verticalSpacer = QSpacerItem(20, 231, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
         self.verticalLayout.addItem(self.verticalSpacer)
 
+
         self.horizontalLayout.addWidget(self.frame)
+
 
         self.retranslateUi(Form)
 
         QMetaObject.connectSlotsByName(Form)
-
     # setupUi
 
     def retranslateUi(self, Form):
         pass
-
     # retranslateUi
+
