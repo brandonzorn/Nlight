f3cb302e72487222356498f9536f3d8ff25f08fa
bzorn <v.klimenko.2137@gmail.com>
2024-04-08 20:54:10 +0300

    removed unused resources and code

--- nlightreader/consts/files/files.py
+++ nlightreader/consts/files/files.py
@@ -4,26 +4,6 @@ from qfluentwidgets import FluentIconBase, Theme, getIconColor
 
 import nlight_res_rc
 
-from PySide6.QtCore import QFile, QTextStream
-
-
-def read_file(file: QFile):
-    file.open(QFile.OpenModeFlag.ReadOnly | QFile.OpenModeFlag.Text)
-    stream = QTextStream(file)
-    return stream.readAll()
-
-
-class Styles:
-    default_widgets = read_file(QFile(":/styles/data/styles/default/widget_default.qss"))
-
-    dark_icons = read_file(QFile(":/styles/data/styles/dark/icons_dark.qss"))
-    dark_widgets = read_file(QFile(":/styles/data/styles/dark/widget_dark.qss"))
-    Dark = dark_icons + default_widgets + dark_widgets
-
-    light_icons = read_file(QFile(":/styles/data/styles/light/icons_light.qss"))
-    light_widgets = read_file(QFile(":/styles/data/styles/light/widget_light.qss"))
-    Light = light_icons + default_widgets + light_widgets
-
 
 class LangIcons:
     Gb = ":/lang_icons/data/icons/lang/gb.svg"
