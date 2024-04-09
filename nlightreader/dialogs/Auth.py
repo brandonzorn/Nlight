71eea588603a062877de5f2a1b09473225ea9227
bzorn <v.klimenko.2137@gmail.com>
2024-04-09 15:40:28 +0300

    optimized code in auth dialog

--- nlightreader/dialogs/Auth.py
+++ nlightreader/dialogs/Auth.py
@@ -18,30 +18,23 @@ class FormAuth(QDialog):
 
     def setup_form(self, fields: int):
         if fields == 1:
-            self.ui.get_code_btn.clicked.connect(self.open_login_page)
-            self.ui.auth_btn.clicked.connect(lambda: self.verify_user_data(fields))
+            self.ui.get_code_btn.clicked.connect(self.__open_login_page)
+            self.ui.auth_btn.clicked.connect(self.verify_user_data)
             self.ui.two_frame.hide()
         else:
-            self.ui.auth_btn.clicked.connect(lambda: self.verify_user_data(fields))
+            self.ui.auth_btn.clicked.connect(self.verify_user_data)
             self.ui.one_frame.hide()
 
-    def verify_user_data(self, fields: int):
-        if fields == 1:
-            code = self.ui.auth_code_line.text()
-            if not code:
-                return
-            self.session.fetch_token(code)
-            if self.session.check_auth():
-                self.accept()
-        else:
-            if self.ui.login_line.text() and self.ui.password_line.text():
-                self.accept()
+    def verify_user_data(self):
+        if (self.ui.login_line.text() and self.ui.password_line.text()) or self.ui.auth_code_line.text():
+            self.accept()
 
     def get_user_data(self):
         return {
             "username": self.ui.login_line.text(),
             "password": self.ui.password_line.text(),
+            "token": self.ui.auth_code_line.text(),
         }
 
-    def open_login_page(self):
+    def __open_login_page(self):
         webbrowser.open_new_tab(self.session.get_auth_url())
