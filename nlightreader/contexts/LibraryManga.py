32efc26a9ba63cacddf49e6f84a1d12bb9f8f422
bzorn <v.klimenko.2137@gmail.com>
2024-04-13 12:39:19 +0300

    replaced QAction with qfluentwidgets Action

--- nlightreader/contexts/LibraryManga.py
+++ nlightreader/contexts/LibraryManga.py
@@ -1,5 +1,4 @@
-from PySide6.QtGui import QAction
-from qfluentwidgets import RoundMenu, FluentIcon
+from qfluentwidgets import RoundMenu, FluentIcon, Action
 
 from nlightreader.utils import translate
 
@@ -7,11 +6,11 @@ from nlightreader.utils import translate
 class LibraryMangaMenu(RoundMenu):
     def __init__(self):
         super().__init__()
-        self.add_to_lib = QAction(FluentIcon.ADD_TO.icon(), translate("Menu", "Add to Library"))
-        self.remove_from_lib = QAction(FluentIcon.REMOVE_FROM.icon(), translate("Menu", "Remove from library"))
-        self.open_in_browser = QAction(FluentIcon.LINK.icon(), translate("Menu", "Open in browser"))
-        self.remove_files = QAction(FluentIcon.DELETE.icon(), translate("Menu", "Clear local files"))
-        self.open_local_files = QAction(FluentIcon.FOLDER.icon(), translate("Menu", "Open local files"))
+        self.add_to_lib = Action(FluentIcon.ADD_TO, translate("Menu", "Add to Library"))
+        self.remove_from_lib = Action(FluentIcon.REMOVE_FROM, translate("Menu", "Remove from library"))
+        self.open_in_browser = Action(FluentIcon.LINK, translate("Menu", "Open in browser"))
+        self.remove_files = Action(FluentIcon.DELETE, translate("Menu", "Clear local files"))
+        self.open_local_files = Action(FluentIcon.FOLDER, translate("Menu", "Open local files"))
 
     def set_mode(self, mode: int):
         """
