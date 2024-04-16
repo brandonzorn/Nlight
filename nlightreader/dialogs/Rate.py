a7bc594764f1402a9d3cfcc6b8759023da413b84
bzorn <v.klimenko.2137@gmail.com>
2024-04-16 22:36:13 +0300

    optimized shikimori_lib requests & updated LibList enum & code refactoring

--- nlightreader/dialogs/Rate.py
+++ nlightreader/dialogs/Rate.py
@@ -2,7 +2,7 @@ from PySide6.QtCore import Slot
 from PySide6.QtWidgets import QDialog, QLayout
 
 from data.ui.dialogs.rate import Ui_Dialog
-from nlightreader.consts.enums import lib_lists_en, parse_lib_list
+from nlightreader.consts.enums import Nl, LIB_LISTS
 from nlightreader.items import Manga
 from nlightreader.utils import translate
 from nlightreader.utils.catalog_manager import get_catalog, get_lib_catalog
@@ -14,7 +14,7 @@ class FormRate(QDialog):
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
         self.layout().setSizeConstraint(QLayout.SizeConstraint.SetFixedSize)
-        self.ui.lib_list_box.addItems([translate("Form", i.capitalize()) for i in lib_lists_en])
+        self.ui.lib_list_box.addItems([translate("Form", i.capitalize()) for i in LIB_LISTS])
         self.ui.update_btn.clicked.connect(self.send_rate)
         self.ui.cancel_btn.clicked.connect(self.close)
         self.ui.delete_btn.clicked.connect(self.delete_rate)
@@ -35,7 +35,7 @@ class FormRate(QDialog):
         self.ui.chapters_box.setValue(self.user_rate.chapters)
         if self.manga.chapters:
             self.ui.chapters_box.setMaximum(self.manga.chapters)
-        self.ui.lib_list_box.setCurrentIndex(lib_lists_en.index(parse_lib_list(self.user_rate.status)))
+        self.ui.lib_list_box.setCurrentIndex(self.user_rate.status.value)
 
     def closeEvent(self, arg__1):
         self.deleteLater()
@@ -44,7 +44,7 @@ class FormRate(QDialog):
     def send_rate(self):
         self.user_rate.score = self.ui.score_box.value()
         self.user_rate.chapters = self.ui.chapters_box.value()
-        self.user_rate.status = lib_lists_en[self.ui.lib_list_box.currentIndex()]
+        self.user_rate.status = Nl.LibList(self.ui.lib_list_box.currentIndex())
         self.catalog.update_user_rate(self.user_rate)
         self.close()
 
