abb2698f11aaaece22bb4283c5dbf2dc9c25fa4d
bzorn <v.klimenko.2137@gmail.com>
2024-04-29 18:51:39 +0300

    updated fpm script

--- makepkg.sh
+++ makepkg.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-DEB_FILE = Nlight.deb
-RPM_FILE = Nlight.rpm
-TAR_FILE = Nlight.tar.zst
+DEB_FILE=Nlight.deb
+RPM_FILE=Nlight.rpm
+TAR_FILE=Nlight.tar.zst
 
 [ -e package ] && rm -r package
 [ -e dist ] && rm -r dist
@@ -12,7 +12,11 @@ TAR_FILE = Nlight.tar.zst
 
 
 . venv/bin/activate
-pyinstaller --noconfirm --windowed --name "Nlight" --key "6w9z$C&F)J@NcQfTjWnZr4u7x!A%D*" "main.py"
+sudo apt install ruby-dev build-essential && sudo gem i fpm -f
+pip install -r requirements.txt
+pip install pyinstaller
+
+pyinstaller --noconfirm --windowed --name "Nlight" "main.py"
 
 mkdir -p package/opt
 mkdir -p package/usr/share/applications
@@ -28,17 +32,14 @@ find package/usr/share -type f -exec chmod 644 -- {} +
 chmod +x package/opt/Nlight/Nlight
 
 
-make_package()
+function make_package()
 {
-  FPM_EXECUTABLE_PATH = /home/bzorn/.local/share/gem/ruby/3.0.0/bin/fpm
-  DESCRIPTION = "Open source manga and ranobe reading application"
-  URL = "https://github.com/brandonzorn/Nlight"
-  MAINTAINER = "brandonzorn <v.klimenko.2137@gmail.com>"
-  PACKAGE_TYPE = $1
-  FILE_NAME = $2
-  $FPM_EXECUTABLE_PATH -C package -s dir -t $PACKAGE_TYPE -n "nlight" -v 1.9.9 --license mit --description $DESCRIPTION  --url $URL --architecture native --maintainer @MAINTAINER -p dist/$FILE_NAME
+  local DESCRIPTION="Open source manga and ranobe reading application"
+  local URL="https://github.com/brandonzorn/Nlight"
+  local MAINTAINER="brandonzorn <v.klimenko.2137@gmail.com>"
+  local PACKAGE_TYPE=$1
+  local FILE_NAME=$2
+  fpm -C package -s dir -t $PACKAGE_TYPE -n "nlight" -p dist/$FILE_NAME --license mit --architecture native --version 1.10.0.5 --description "$DESCRIPTION" --url "$URL" --maintainer "$MAINTAINER"
 }
 
 make_package deb $DEB_FILE
-make_package rpm $RPM_FILE
-make_package pacman $TAR_FILE
