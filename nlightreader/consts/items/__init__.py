3d7fd439f0f694b0fadf582dd73568becf7f4e7c
bzorn <v.klimenko.2137@gmail.com>
2024-05-10 20:11:43 +0300

    added filters and translation type name

--- nlightreader/consts/items/__init__.py
+++ nlightreader/consts/items/__init__.py
@@ -2,5 +2,5 @@ from .desu_items import DesuItems
 from .ranobehub_items import RanobehubItems
 from .remanga_items import RemangaItems
 from .rulate_items import RulateItems
-from .shikimori_items import ShikimoriItems
+from .shikimori_items import ShikimoriItems, ShikimoriAnimeItems
 from .mangalib_items import MangaLibItems
