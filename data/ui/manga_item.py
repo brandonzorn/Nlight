d6e8994dbf37e3989c4857b8719cf978949195ec
bzorn <v.klimenko.2137@gmail.com>
2024-05-11 20:31:05 +0300

    updated manga items and manga grid

--- data/ui/manga_item.py
+++ data/ui/manga_item.py
@@ -15,34 +15,45 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QLabel, QSizePolicy, QSpacerItem,
-    QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QLabel, QSizePolicy, QVBoxLayout,
+    QWidget)
 
-from qfluentwidgets import BodyLabel
+from qfluentwidgets import (BodyLabel, CardWidget)
 
 class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(550, 486)
+        Form.resize(400, 380)
         Form.setContextMenuPolicy(Qt.CustomContextMenu)
-        self.verticalLayout = QVBoxLayout(Form)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.image = QLabel(Form)
+        self.verticalLayout_2 = QVBoxLayout(Form)
+        self.verticalLayout_2.setSpacing(3)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.image_card = CardWidget(Form)
+        self.image_card.setObjectName(u"image_card")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.image_card.sizePolicy().hasHeightForWidth())
+        self.image_card.setSizePolicy(sizePolicy)
+        self.verticalLayout_3 = QVBoxLayout(self.image_card)
+        self.verticalLayout_3.setSpacing(0)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.image = QLabel(self.image_card)
         self.image.setObjectName(u"image")
         self.image.setAlignment(Qt.AlignHCenter|Qt.AlignTop)
 
-        self.verticalLayout.addWidget(self.image)
+        self.verticalLayout_3.addWidget(self.image)
 
-        self.verticalSpacer = QSpacerItem(20, 412, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
-        self.verticalLayout.addItem(self.verticalSpacer)
+        self.verticalLayout_2.addWidget(self.image_card)
 
         self.name_lbl = BodyLabel(Form)
         self.name_lbl.setObjectName(u"name_lbl")
-        self.name_lbl.setWordWrap(True)
 
-        self.verticalLayout.addWidget(self.name_lbl)
+        self.verticalLayout_2.addWidget(self.name_lbl)
 
 
         self.retranslateUi(Form)
