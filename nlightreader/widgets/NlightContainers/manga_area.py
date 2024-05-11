ffff8ab84284e668fc47466c2f2062a2a42527a7
bzorn <v.klimenko.2137@gmail.com>
2024-05-11 20:31:05 +0300

    updated manga items and manga grid

--- nlightreader/widgets/NlightContainers/manga_area.py
+++ nlightreader/widgets/NlightContainers/manga_area.py
@@ -18,18 +18,19 @@ class MangaArea(ScrollArea, AbstractContentContainer):
             """,
         )
         self._column_count = 5
+        self._spacing = 12
         self._manga_items: list[MangaItem] = []
 
         self._scrollAreaWidgetContents = QWidget()
         self._scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
-        self._scrollAreaWidgetContents.resizeEvent = self._scroll_resize_event
 
         self._scroll_layout = QHBoxLayout(self._scrollAreaWidgetContents)
         self._scroll_layout.setSpacing(0)
         self._scroll_layout.setContentsMargins(0, 0, 0, 0)
 
         self._content_grid = QGridLayout()
-        self._content_grid.setVerticalSpacing(12)
+        self._content_grid.setSpacing(self._spacing)
+        self._content_grid.setContentsMargins(0, 0, 24, 0)
         self._content_grid.setAlignment(Qt.AlignmentFlag.AlignTop)
 
         self._scroll_layout.addLayout(self._content_grid)
@@ -40,10 +41,11 @@ class MangaArea(ScrollArea, AbstractContentContainer):
         self.manga_thread_pool.setMaxThreadCount(self._column_count)
         self._set_images_thread = Thread(target=self.partial_image_addition)
 
-    def _scroll_resize_event(self, event):
-        if event.oldSize().width() != event.size().width():
+    def resizeEvent(self, arg__1):
+        super().resizeEvent(arg__1)
+        if arg__1.oldSize().width() != arg__1.size().width():
+            self._scrollAreaWidgetContents.setFixedWidth(arg__1.size().width())
             self.update_items()
-        event.accept()
 
     def add_items(self, items: list[MangaItem]):
         i, j = 0, 0
@@ -71,7 +73,9 @@ class MangaArea(ScrollArea, AbstractContentContainer):
         self._manga_items.clear()
 
     def update_items(self):
-        size = self.size().width() // (self._column_count + 1)
+        size = (self.size().width() - (
+                self._content_grid.horizontalSpacing() * self._column_count)
+                ) // self._column_count
         [item.set_size(size) for item in self._manga_items]
 
     def get_content_widget(self):
