b9a3ca01f17f57bb74289fe9783306d7ff28d3c2
bzorn <v.klimenko.2137@gmail.com>
2024-05-18 08:43:00 +0300

    flake8

--- nlightreader/utils/kodik_server.py
+++ nlightreader/utils/kodik_server.py
@@ -7,15 +7,15 @@ from nlightreader.utils import Database
 
 class KodikHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
+        content_length = int(self.headers["Content-Length"])
         post_data = self.rfile.read(content_length)
         data = json.loads(post_data)
 
-        manga_id = data.get('manga_id', '')
-        chapter_id = data.get('chapter_id', '')
-        # player_cur_time = data.get('player_cur_time', '')
-        # player_max_time = data.get('player_max_time', '')
-        is_completed = bool(data.get('is_completed', False))
+        manga_id = data.get("manga_id", "")
+        chapter_id = data.get("chapter_id", "")
+        # player_cur_time = data.get("player_cur_time", "")
+        # player_max_time = data.get("player_max_time", "")
+        is_completed = bool(data.get("is_completed", False))
 
         db = Database()
         manga = db.get_manga(manga_id)
@@ -23,16 +23,16 @@ class KodikHTTPRequestHandler(BaseHTTPRequestHandler):
         note = HistoryNote(chapter, manga, is_completed)
         db.add_history_note(note)
 
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'POST')
-        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "POST")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
 
         self.send_response(200)
         self.end_headers()
 
     def do_OPTIONS(self):
         self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'POST')
-        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "POST")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
         self.end_headers()
