c3e21f923fb99f7c6e303eff4c357d57f7875240
bzorn <v.klimenko.2137@gmail.com>
2024-05-19 18:45:24 +0300

    added new Lib parsers

---
+++ nlightreader/parsers/combined/lib/lib_mangalib.py
@@ -0,0 +1,15 @@
+from nlightreader.consts.urls import URL_MANGALIB
+from nlightreader.parsers.catalogs_base import AbstractMangaCatalog
+from nlightreader.parsers.combined.lib.lib_base import LibBase
+
+
+class LibMangalib(LibBase, AbstractMangaCatalog):
+    CATALOG_NAME = "MangaLib"
+    CATALOG_ID = 14
+
+    def __init__(self):
+        super().__init__()
+        self.url = URL_MANGALIB
+
+        self.content_name = "manga"
+        self.site_id = 1
