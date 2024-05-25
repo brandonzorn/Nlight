9b7922e42056eaed30a1d2a216259ca2c66d8daf
bzorn <v.klimenko.2137@gmail.com>
2024-05-25 13:09:40 +0300

    added anilib, ranobelib, mangalib filters

---
+++ nlightreader/consts/items/ranobelib_items.py
@@ -0,0 +1,27 @@
+from nlightreader.consts.items.lib_base_items import LibBaseItems
+from nlightreader.consts.items.preset_items import PresetKinds as Pk, PresetOrders as Po
+
+
+class RanobeLibItems(LibBaseItems):
+    ORDERS = [
+        {"value": None} | Po.POPULARITY,
+        {"value": "rate_avg"} | Po.RATING,
+        {"value": "views"} | Po.VIEWS,
+        {"value": "chap_count"} | Po.CHAPTERS_COUNT,
+
+        {"value": "releaseDate"} | Po.AIRED_ON,
+        {"value": "last_chapter_at"} | Po.UPDATED,
+        {"value": "created_at"} | Po.CREATED,
+
+        {"value": "name"} | Po.NAME,
+        {"value": "rus_name"} | Po.RUS_NAME,
+    ]
+
+    KINDS = [
+        {"value": 10, "name": "Japan", "russian": "Япония"},
+        {"value": 11, "name": "Korea", "russian": "Корея"},
+        {"value": 12, "name": "China", "russian": "Китай"},
+        {"value": 13, "name": "English", "russian": "Английский"},
+        {"value": 14, "name": "Original", "russian": "Авторский"},
+        {"value": 15, "name": "Fanfiction", "russian": "Фанфик"},
+    ]
