10efb15881cac1a7fa602d6711884493fea40a74
bzorn <v.klimenko.2137@gmail.com>
2024-07-01 22:08:04 +0300

    updated mangalib parser (added images)

--- nlightreader/parsers/combined/lib/lib_mangalib.py
+++ nlightreader/parsers/combined/lib/lib_mangalib.py
@@ -1,7 +1,9 @@
 from nlightreader.consts.items import MangaLibItems
 from nlightreader.consts.urls import URL_MANGALIB
+from nlightreader.items import Manga, Chapter, Image
 from nlightreader.parsers.catalogs_base import AbstractMangaCatalog
 from nlightreader.parsers.combined.lib.lib_base import LibBase
+from nlightreader.utils.utils import get_html
 
 
 class LibMangalib(LibBase, AbstractMangaCatalog):
@@ -15,3 +17,26 @@ class LibMangalib(LibBase, AbstractMangaCatalog):
 
         self.content_name = "manga"
         self.site_id = 1
+
+    def get_images(self, manga: Manga, chapter: Chapter) -> list[Image]:
+        url = f"{self.url_api}/{self.content_name}/{manga.content_id}/chapter?number={chapter.ch}&volume={chapter.vol}"
+        response = get_html(url, content_type="json")
+        images = []
+        if response:
+            data = response["data"]
+            for i, page_data in enumerate(data.get("pages", [])):
+                images.append(
+                    Image(
+                        page_data["id"],
+                        i + 1,
+                        f"https://img33.imgslib.link{page_data['url']}",
+                    ),
+                )
+        return images
+
+    def get_image(self, image: Image):
+        return get_html(image.img, content_type="content")
+
+
+    def get_manga_url(self, manga: Manga) -> str:
+        return f"{self.url}/ru/manga/{manga.content_id}"
