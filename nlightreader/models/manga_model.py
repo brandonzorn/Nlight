3170ae279079a0eff42b3102f9f3bcf855080af8
bzorn <v.klimenko.2137@gmail.com>
2024-07-13 14:59:38 +0300

    flake8 + black code style

--- nlightreader/models/manga_model.py
+++ nlightreader/models/manga_model.py
@@ -10,11 +10,11 @@ from nlightreader.models.base_model import BaseModel
 
 class Manga(BaseModel):
     def __init__(
-            self,
-            content_id: str,
-            catalog_id: int,
-            name: str,
-            russian: str,
+        self,
+        content_id: str,
+        catalog_id: int,
+        name: str,
+        russian: str,
     ):
         super().__init__(content_id, catalog_id)
 
