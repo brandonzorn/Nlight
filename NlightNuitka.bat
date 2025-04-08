6bc9c260cd4999e1b7531a953d7aeb7e7f22f5a6
bzorn <v.klimenko.2137@gmail.com>
2025-04-08 21:13:44 +0300

    Reformat build scripts

--- NlightNuitka.bat
+++ NlightNuitka.bat
@@ -1,1 +1,13 @@
-nuitka --onefile --disable-console --follow-imports --enable-plugin=pyside6 --product-version="1.10.0.2" --file-version="1.10.0.2" --company-name="brandonzorn" --product-name="Nlight" --windows-icon-from-ico="pkg_res/Nlight.ico" -o "Nlight" --output-dir=build_nuitka/ main.py
+nuitka ^
+  --onefile ^
+  --windows-console-mode=disable ^
+  --follow-imports ^
+  --enable-plugin=pyside6 ^
+  --product-version="1.10.4.0" ^
+  --file-version="1.10.4.0" ^
+  --company-name="brandonzorn" ^
+  --product-name="Nlight" ^
+  --windows-icon-from-ico="pkg_res/Nlight.ico" ^
+  -o "Nlight" ^
+  --output-dir=build_nuitka/ ^
+  main.py
