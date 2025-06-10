a564a6ebb572b04090ffefd11952e0c7976cff4e
bzorn <v.klimenko.2137@gmail.com>
2025-06-10 23:29:21 +0300

    feat: remove catalog_id from CharacterInfoDialog constructor

--- nlightreader/widgets/dialogs/character_info_dialog.py
+++ nlightreader/widgets/dialogs/character_info_dialog.py
@@ -2,6 +2,7 @@ from PySide6.QtCore import QSize, Slot
 from PySide6.QtWidgets import QDialog
 
 from data.ui.dialogs.character import Ui_CharacterInfoDialog
+from nlightreader.models import Character
 from nlightreader.utils.catalog_manager import get_catalog_by_id
 from nlightreader.utils.file_manager import FileManager
 from nlightreader.utils.text_formatter import description_to_html
@@ -9,7 +10,7 @@ from nlightreader.utils.threads import Worker
 
 
 class CharacterInfoDialog(QDialog):
-    def __init__(self, character, catalog_id, parent=None):
+    def __init__(self, character: Character, parent=None):
         super().__init__(parent)
         self.ui = Ui_CharacterInfoDialog()
         self.ui.setupUi(self)
@@ -17,7 +18,7 @@ class CharacterInfoDialog(QDialog):
         self.setFixedSize(QSize(550, 800))
         self.__character = character
         self.setWindowTitle(self.__character.get_name())
-        self.__catalog = get_catalog_by_id(catalog_id)
+        self.__catalog = get_catalog_by_id(character.catalog_id)
         self.ui.name_label.setText(self.__character.name)
         self.ui.russian_label.setText(self.__character.russian)
         self.update_description()
