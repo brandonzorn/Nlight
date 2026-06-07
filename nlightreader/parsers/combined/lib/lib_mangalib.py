3565b08c51dbc70dd4d2ac5af46b2fdbd1cd4050
bzorn <v.klimenko.2137@gmail.com>
2026-06-07 23:41:22 +0300

    feat: improve Lib parsers reliability and update mirrors

--- nlightreader/parsers/combined/lib/lib_mangalib.py
+++ nlightreader/parsers/combined/lib/lib_mangalib.py
@@ -1,3 +1,5 @@
+from typing import override
+
 from nlightreader.consts.items import MangaLibItems
 from nlightreader.models import Chapter, Image, Manga
 from nlightreader.parsers.catalogs_base import AbstractMangaCatalog
@@ -10,10 +12,12 @@ class LibMangalib(LibBase, AbstractMangaCatalog):
     CATALOG_ID = 10
     _FILTERS = MangaLibItems
     _URL = "https://mangalib.me"
+    _DEFAULT_IMG_HOST = "https://img2.imglib.info"
 
     _CONTENT_NAME = "manga"
     _SITE_ID = 1
 
+    @override
     def get_images(self, manga: Manga, chapter: Chapter) -> list[Image]:
         url = (
             f"{self._URL_API}/{self._CONTENT_NAME}/{manga.content_id}/chapter"
@@ -22,31 +26,36 @@ class LibMangalib(LibBase, AbstractMangaCatalog):
             "number": chapter.chapter_number,
             "volume": chapter.volume_number,
         }
+
         response = get_html(url, params=params, content_type="json")
-        images = []
-        if response:
-            data = response["data"]
-            for i, page_data in enumerate(data.get("pages", [])):
-                images.append(
-                    Image(
-                        page_data["id"],
-                        i + 1,
-                        f"https://img33.imgslib.link{page_data['url']}",
-                    ),
-                )
+        images: list[Image] = []
+
+        if not isinstance(response, dict):
+            return images
+
+        data = response.get("data", {})
+        for i, page_data in enumerate(data.get("pages", [])):
+            if not isinstance(page_data, dict):
+                continue
+            page_url = page_data.get("url")
+            if not page_url:
+                continue
+
+            full_url = f"{self._DEFAULT_IMG_HOST}{page_url}"
+            images.append(Image(page_data["id"], i + 1, full_url))
         return images
 
-    def get_image(self, image: Image):
+    @override
+    def get_image(self, image: Image) -> bytes | None:
         return get_html(
             image.url,
+            headers=self._headers,
             content_type="content",
-            headers={"Referer": f"{self._URL}/"},
         )
 
+    @override
     def get_manga_url(self, manga: Manga) -> str:
         return f"{self._URL}/ru/manga/{manga.content_id}"
 
 
-__all__ = [
-    "LibMangalib",
-]
+__all__ = ["LibMangalib"]
