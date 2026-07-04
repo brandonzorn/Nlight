bd095b99e604c55922e8a380e6e25841204a4bb0
bzorn <v.klimenko.2137@gmail.com>
2026-07-04 15:14:38 +0300

    feat: update default headers

--- nlightreader/consts/urls.py
+++ nlightreader/consts/urls.py
@@ -3,8 +3,8 @@ URL_SHIKIMORI_API = f"{URL_SHIKIMORI}/api"
 URL_SHIKIMORI_TOKEN = f"{URL_SHIKIMORI}/oauth/token"
 
 DEFAULT_HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0)"
-    "Gecko/20100101 Firefox/99.0",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:152.0) "
+    "Gecko/20100101 Firefox/152.0",
 }
 
 SHIKIMORI_HEADERS = {"User-Agent": "Nlight"}
