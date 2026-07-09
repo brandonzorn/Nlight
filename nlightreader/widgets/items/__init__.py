e48a2a4c4b275b0eaf01ea13c907db0b71ca83c4
bzorn <v.klimenko.2137@gmail.com>
2026-07-09 17:55:45 +0300

    fix: remove old imports

--- nlightreader/widgets/items/__init__.py
+++ nlightreader/widgets/items/__init__.py
@@ -2,7 +2,7 @@ from .group_tree_item import GroupTreeItem
 from .manga_item import MangaItem
 from .title_tree_item import ChapterTreeItem
 
-__all__ = ["MangaItem", "ChapterTreeItem"]
+__all__ = [
     "GroupTreeItem",
     "MangaItem",
 ]
