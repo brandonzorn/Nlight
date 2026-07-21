eb356d2221bb4f5c18f5306e91bac8c13de1010c
bzorn <v.klimenko.2137@gmail.com>
2026-07-21 17:49:25 +0300

    fix: add default value to ParserItems

--- nlightreader/consts/items/parser_items.py
+++ nlightreader/consts/items/parser_items.py
@@ -2,9 +2,9 @@ from abc import ABC
 
 
 class ParserItems(ABC):
-    ORDERS: tuple[dict[str, str], ...]
-    KINDS: tuple[dict[str, str], ...]
-    GENRES: tuple[dict[str, str], ...]
+    ORDERS: tuple[dict[str, str], ...] = ()
+    KINDS: tuple[dict[str, str], ...] = ()
+    GENRES: tuple[dict[str, str], ...] = ()
 
 
 __all__ = ["ParserItems"]
