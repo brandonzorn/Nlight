ce44f3531f7692ee982e823021a4a36098a9653f
bzorn <v.klimenko.2137@gmail.com>
2026-08-03 21:39:39 +0300

    fix: add missing qttr imports

--- data/ui/widgets/facial.py
+++ data/ui/widgets/facial.py
@@ -9,8 +9,8 @@
 ################################################################################
 
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
+                            QMetaObject, QObject, QPoint, QRect,
+                            QSize, QTime, QUrl, Qt, qtTrId)
 from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
